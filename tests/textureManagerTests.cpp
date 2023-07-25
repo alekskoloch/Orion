025e1f6152d845a492986445282fa17264cb1297
Aleks <aleks.koloch@gmail.com>
2023-07-25 23:11:57 +0200

    Add tests for TextureManager

--- tests/textureManagerTests.cpp
+++ tests/textureManagerTests.cpp
@@ -1,3 +1,28 @@
 #include <gtest/gtest.h>
 
 #include "../src/managers/TextureManager.h"
+
+TEST(TextureManagerTest, LoadTextureCorrectly)
+{
+    TextureManager& tm = TextureManager::getInstance();
+    EXPECT_NO_THROW(tm.loadTexture("correct_texture", ASSETS_PATH + std::string("player.png")));
+}
+
+TEST(TextureManagerTest, ThrowsWhenLoadingNonexistentTexture)
+{
+    TextureManager& tm = TextureManager::getInstance();
+    EXPECT_THROW(tm.loadTexture("nonexistent_texture", ASSETS_PATH + std::string("wrong_player.png")), std::runtime_error);
+}
+
+TEST(TextureManagerTest, GetTextureCorrectly)
+{
+    TextureManager& tm = TextureManager::getInstance();
+    tm.loadTexture("test_texture", ASSETS_PATH + std::string("player.png"));
+    EXPECT_NO_THROW(tm.getTexture("test_texture"));
+}
+
+TEST(TextureManagerTest, ThrowsWhenGettingNonexistentTexture)
+{
+    TextureManager& tm = TextureManager::getInstance();
+    EXPECT_THROW(tm.getTexture(ASSETS_PATH + std::string("wrong_player.png")), std::out_of_range);
+}
