3429b4561a7768f424cc8163bb50aa6c43896f55
Aleks <aleks.koloch@gmail.com>
2023-07-25 18:48:16 +0200

    Add unordered map to TextureManager class

--- src/managers/TextureManager.h
+++ src/managers/TextureManager.h
@@ -1,1 +1,14 @@
 #pragma once
+
+#include <unordered_map>
+#include <string>
+
+#include <SFML/Graphics.hpp>
+
+class TextureManager
+{
+private:
+    std::unordered_map<std::string, sf::Texture> textures;
+public:
+
+};
