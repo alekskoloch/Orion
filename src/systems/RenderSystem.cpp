80276e4caa197e5aafe521c59f1c2acbb11d3d58
Aleks <aleks.koloch@gmail.com>
2023-07-25 21:05:26 +0200

    Add renderEntities member function to RenderSystem

--- src/systems/RenderSystem.cpp
+++ src/systems/RenderSystem.cpp
@@ -1,1 +1,14 @@
 #include "RenderSystem.h"
+
+#include "../components/components.h"
+
+void RenderSystem::renderEntities(sf::RenderWindow& window, entt::registry& registry)
+{
+    auto view = registry.view<Renderable, Position>();
+    for (auto entity : view)
+    {
+        auto [position, renderable] = view.get<Position, Renderable>(entity);
+        renderable.sprite.setPosition(position.position);
+        window.draw(renderable.sprite);
+    }
+}
--- src/systems/RenderSystem.h
+++ src/systems/RenderSystem.h
@@ -1,1 +1,10 @@
 #pragma once
+
+#include <SFML/Graphics.hpp>
+#include <entt/entt.hpp>
+
+class RenderSystem
+{
+public:
+    static void renderEntities(sf::RenderWindow& window, entt::registry& registry);
+};
