9ac6984e9e952f19db37da800b3877c8e0d30c61
Aleks <aleks.koloch@gmail.com>
2023-07-27 22:32:35 +0200

    Update MovementSystem to support only position update

--- src/systems/MovementSystem.cpp
+++ src/systems/MovementSystem.cpp
@@ -6,25 +6,12 @@
 
 void MovementSystem::updateMovement(entt::registry& registry, sf::Time deltaTime)
 {
-    auto view = registry.view<Position, Input, Speed>();
+    auto view = registry.view<Position, Velocity>();
     for (auto entity : view)
     {
         auto& position = view.get<Position>(entity);
-        auto& input = view.get<Input>(entity);
-        auto& speed = view.get<Speed>(entity);
+        auto& velocity = view.get<Velocity>(entity);
 
-        sf::Vector2f direction(0.f, 0.f);
-
-        if (input.up)
-            direction.y -= 1.f;
-        if (input.down)
-            direction.y += 1.f;;
-        if (input.left)
-            direction.x -= 1.f;
-        if (input.right)
-            direction.x += 1.f;
-
-        //TODO: Velocity
-        position.position += direction * speed.speed * deltaTime.asSeconds();
+        position.position += velocity.velocity * deltaTime.asSeconds();
     }
 }
