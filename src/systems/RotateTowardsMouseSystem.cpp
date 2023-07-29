8f8678560659c1b0b346eb209bcd79cfc69184e8
Aleks <aleks.koloch@gmail.com>
2023-07-29 14:32:39 +0200

    Refactor rotation logic into separate function

--- src/systems/RotateTowardsMouseSystem.cpp
+++ src/systems/RotateTowardsMouseSystem.cpp
@@ -6,6 +6,30 @@
 
 #include <iostream>
 
+float DistanceToMouse(const sf::Vector2f& mousePosition, const Position& position)
+{
+    return std::sqrt(std::pow(mousePosition.x - position.position.x, 2) + std::pow(mousePosition.y - position.position.y, 2));
+}
+
+float TargetAngle(const sf::Vector2f& mousePosition, const Position& position, float offset = 90.0f)
+{
+    return std::atan2(mousePosition.y - position.position.y, mousePosition.x - position.position.x) * (180 / M_PI) + offset;
+}
+
+float AngleDifference(float targetAngle, float currentAngle)
+{
+    float angleDifference = targetAngle - currentAngle;
+    if (angleDifference > 180.0f)
+    {
+        angleDifference -= 360.0f;
+    }
+    else if (angleDifference < -180.0f)
+    {
+        angleDifference += 360.0f;
+    }
+    return angleDifference;
+}
+
 void RotateTowardsMouseSystem::rotateTowardsMouse(entt::registry& registry, sf::Time deltaTime, sf::RenderWindow& window)
 {
     auto view = registry.view<RotationTowardsMouse, Position, Renderable>();
@@ -19,22 +43,13 @@ void RotateTowardsMouseSystem::rotateTowardsMouse(entt::registry& registry, sf::
         if (rotation.enabled)
         {
             sf::Vector2f mousePosition = static_cast<sf::Vector2f>(sf::Mouse::getPosition(window));
-            float distanceToMouse = std::sqrt(std::pow(mousePosition.x - position.position.x, 2) + std::pow(mousePosition.y - position.position.y, 2));
+            float distanceToMouse = DistanceToMouse(mousePosition, position);
 
             if (distanceToMouse > rotation.minimalActivationDistance)
             {
-                float targetAngle = std::atan2(mousePosition.y - position.position.y, mousePosition.x - position.position.x) * (180 / 3.14159265f) + 90;
+                float targetAngle = TargetAngle(mousePosition, position);
                 float currentAngle = renderable.sprite.getRotation();
-                float angleDifference = targetAngle - currentAngle;
-
-                if (angleDifference > 180.0f)
-                {
-                    angleDifference -= 360.0f;
-                }
-                else if (angleDifference < -180.0f)
-                {
-                    angleDifference += 360.0f;
-                }
+                float angleDifference = AngleDifference(targetAngle, currentAngle);
 
                 float rotationAmount = angleDifference;
                 if (rotationAmount > rotation.sensitivity * deltaTime.asSeconds())
