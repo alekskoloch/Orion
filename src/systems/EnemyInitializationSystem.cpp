fac1101b09f243249b6dc67b8b05c5c34ef87677
Aleks <aleks.koloch@gmail.com>
2023-07-29 22:45:59 +0200

    Update enemy initialization to support waypoints

--- src/systems/EnemyInitializationSystem.cpp
+++ src/systems/EnemyInitializationSystem.cpp
@@ -5,7 +5,7 @@
 #include "../components/tagComponents.h"
 
 //TODO: Only for testing
-static float enemyStartPositionX = 1820.f;
+static float enemyStartPositionX = 100.f;
 static float enemyStartPositionY = 980.f;
 
 void EnemyInitializationSystem::initializeEnemy(entt::registry& registry)
@@ -19,4 +19,15 @@ void EnemyInitializationSystem::initializeEnemy(entt::registry& registry)
     sprite.setOrigin(sprite.getGlobalBounds().width / 2.f, sprite.getGlobalBounds().height / 2.f);
     registry.emplace<Renderable>(enemy, sprite);
     registry.emplace<Position>(enemy, sf::Vector2f(enemyStartPositionX, enemyStartPositionY));
+    registry.emplace<Speed>(enemy, 600.f);
+    registry.emplace<Velocity>(enemy, sf::Vector2f(0.f, 0.f));
+
+    std::vector<sf::Vector2f> waypoints = {
+    sf::Vector2f(100.f, 100.f),
+    sf::Vector2f(1820.f, 100.f),
+    sf::Vector2f(1820.f, 980.f),
+    sf::Vector2f(100.f, 980.f)
+    };
+
+    registry.emplace<WaypointMovement>(enemy, waypoints);
 }
