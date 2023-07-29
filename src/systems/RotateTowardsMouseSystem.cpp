852031200329472ade56e6d7ca99bed3ea910188
Aleks <aleks.koloch@gmail.com>
2023-07-29 13:50:15 +0200

    Add rotateTowardsMouse member function

--- src/systems/RotateTowardsMouseSystem.cpp
+++ src/systems/RotateTowardsMouseSystem.cpp
@@ -1,1 +1,53 @@
 #include "RotateTowardsMouseSystem.h"
+
+#include <cmath>
+
+#include "../components/components.h"
+
+#include <iostream>
+
+void RotateTowardsMouseSystem::rotateTowardsMouse(entt::registry& registry, sf::Time deltaTime, sf::RenderWindow& window)
+{
+    auto view = registry.view<RotationTowardsMouse, Position, Renderable>();
+
+    for (auto entity : view)
+    {
+        auto& rotation = view.get<RotationTowardsMouse>(entity);
+        auto& position = view.get<Position>(entity);
+        auto& renderable = view.get<Renderable>(entity);
+
+        if (rotation.enabled)
+        {
+            sf::Vector2f mousePosition = static_cast<sf::Vector2f>(sf::Mouse::getPosition(window));
+            float distanceToMouse = std::sqrt(std::pow(mousePosition.x - position.position.x, 2) + std::pow(mousePosition.y - position.position.y, 2));
+
+            if (distanceToMouse > rotation.minimalActivationDistance)
+            {
+                float targetAngle = std::atan2(mousePosition.y - position.position.y, mousePosition.x - position.position.x) * (180 / 3.14159265f) + 90;
+                float currentAngle = renderable.sprite.getRotation();
+                float angleDifference = targetAngle - currentAngle;
+
+                if (angleDifference > 180.0f)
+                {
+                    angleDifference -= 360.0f;
+                }
+                else if (angleDifference < -180.0f)
+                {
+                    angleDifference += 360.0f;
+                }
+
+                float rotationAmount = angleDifference;
+                if (rotationAmount > rotation.sensitivity * deltaTime.asSeconds())
+                {
+                    rotationAmount = rotation.sensitivity * deltaTime.asSeconds();
+                }
+                else if (rotationAmount < -rotation.sensitivity * deltaTime.asSeconds())
+                {
+                    rotationAmount = -rotation.sensitivity * deltaTime.asSeconds();
+                }
+
+                renderable.sprite.setRotation(currentAngle + rotationAmount);
+            }
+        }
+    }
+}
--- src/systems/RotateTowardsMouseSystem.h
+++ src/systems/RotateTowardsMouseSystem.h
@@ -1,1 +1,10 @@
 #pragma once
+
+#include <SFML/Graphics.hpp>
+#include <entt/entt.hpp>
+
+class RotateTowardsMouseSystem
+{
+public:
+    static void rotateTowardsMouse(entt::registry& registry, sf::Time deltaTime, sf::RenderWindow& window);
+};
