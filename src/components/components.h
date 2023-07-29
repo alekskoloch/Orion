11fc1af66a166e803944f141cd40ac30b8c3744f
Aleks <aleks.koloch@gmail.com>
2023-07-29 22:40:22 +0200

    Add WaypointMovement component

--- src/components/components.h
+++ src/components/components.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <vector>
+
 #include <SFML/Graphics.hpp>
 
 struct Acceleration
@@ -41,4 +43,13 @@ struct Speed
 struct Velocity
 {
     sf::Vector2f velocity;
+};
+
+struct WaypointMovement
+{
+    std::vector<sf::Vector2f> waypoints;
+    unsigned int currentWaypointIndex;
+
+    WaypointMovement(std::vector<sf::Vector2f> waypoints, unsigned int currentWaypointIndex = 0)
+        : waypoints(waypoints), currentWaypointIndex(currentWaypointIndex) {}
 };
