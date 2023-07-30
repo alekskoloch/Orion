f9535a6829834fc286ef3f710c3fb6defa2c6384
Aleks <aleks.koloch@gmail.com>
2023-07-30 20:10:37 +0200

    Update SystemManager to support ShootingSystem

--- src/managers/SystemManager.cpp
+++ src/managers/SystemManager.cpp
@@ -3,12 +3,15 @@
 #include "../systems/PlayerInitializationSystem.h"
 #include "../systems/EnemyInitializationSystem.h"
 
-#include "../systems/AccelerationSystem.h"
 #include "../systems/InputSystem.h"
-#include "../systems/MovementSystem.h"
-#include "../systems/RenderSystem.h"
+
 #include "../systems/RotateTowardsMouseSystem.h"
 #include "../systems/WaypointsMovementSystem.h"
+#include "../systems/ShootingSystem.h"
+#include "../systems/AccelerationSystem.h"
+#include "../systems/MovementSystem.h"
+
+#include "../systems/RenderSystem.h"
 
 SystemManager::SystemManager(sf::RenderWindow& window, entt::registry& registry) : window(window), registry(registry) {}
 
@@ -27,6 +30,7 @@ void SystemManager::executeUpdateSystems(sf::Time deltaTime)
 {
     RotateTowardsMouseSystem::rotateTowardsMouse(this->registry, deltaTime, this->window);
     WaypointsMovementSystem::updateWaypoints(this->registry, deltaTime);
+    ShootingSystem::shoot(this->registry, deltaTime);
     AccelerationSystem::accelerate(this->registry, deltaTime);
     MovementSystem::updateMovement(this->registry, deltaTime);
 }
