44f7595702cc3b684c53dfe8260773798762df78
Aleks <aleks.koloch@gmail.com>
2023-07-30 20:16:30 +0200

    Add WeaponsSchema.h file

---
+++ src/schema/WeaponsSchema.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "../components/components.h"
+
+struct WeaponSchema
+{
+    float cooldownTime;
+    float bulletSpeed;
+    std::string bulletTextureName;
+};
+
+static WeaponSchema blueWeapon =
+{
+    0.4f,
+    2000.f,
+    "bule_bullet"
+};
+
+static WeaponSchema redWeapon =
+{
+    0.2f,
+    1300.f,
+    "red_bullet"
+};
