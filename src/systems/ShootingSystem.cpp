75c4f7893d3997156aad719438c41435c78e873e
Aleks <aleks.koloch@gmail.com>
2023-07-30 20:08:06 +0200

    Add shoot function to ShootingSystem

--- src/systems/ShootingSystem.cpp
+++ src/systems/ShootingSystem.cpp
@@ -1,1 +1,62 @@
 #include "ShootingSystem.h"
+
+#include "ShootingSystem.h"
+
+#include "../managers/TextureManager.h"
+#include "../components/components.h"
+
+#include <iostream>
+
+void ShootingSystem::shoot(entt::registry& registry, sf::Time deltaTime)
+{
+    auto view = registry.view<Weapon, Input, Position, Renderable>();
+    for (auto entity : view)
+    {
+        auto& weapon = view.get<Weapon>(entity);
+        auto& input = view.get<Input>(entity);
+        auto& playerPosition = view.get<Position>(entity);
+        auto& playerRenderable = view.get<Renderable>(entity);
+
+        bool canShoot = false;
+
+        if (weapon.autofire)
+        {
+            canShoot = input.shoot && weapon.currentCooldownTime == 0.f;
+        }
+        else
+        {
+            canShoot = input.shoot && !weapon.shootLastFrame;
+        }
+
+        if (weapon.currentCooldownTime > 0.f)
+        {
+            weapon.currentCooldownTime -= deltaTime.asSeconds();
+            if (weapon.currentCooldownTime < 0.f)
+                weapon.currentCooldownTime = 0.f;
+        }
+
+        if (canShoot)
+        {
+            auto bulletEntity = registry.create();
+
+            sf::Sprite sprite(TextureManager::getInstance().getTexture(weapon.bulletTextureName));
+            sprite.setOrigin(sprite.getGlobalBounds().width / 2.f, sprite.getGlobalBounds().height / 2.f);
+
+            registry.emplace<Position>(bulletEntity, playerPosition.position);
+
+            float rotation = playerRenderable.sprite.getRotation();
+            sprite.setRotation(rotation);
+
+            float adjustedRotation = 90.f - rotation;
+            float radians = adjustedRotation * (3.14159265f / 180.f);
+            sf::Vector2f velocity(std::cos(radians) * weapon.bulletSpeed, -std::sin(radians) * weapon.bulletSpeed);
+
+            registry.emplace<Velocity>(bulletEntity, velocity);
+            registry.emplace<Renderable>(bulletEntity, sprite);
+
+            weapon.currentCooldownTime = weapon.cooldownTime;
+        }
+
+        weapon.shootLastFrame = input.shoot;
+    }
+}
--- src/systems/ShootingSystem.h
+++ src/systems/ShootingSystem.h
@@ -1,1 +1,10 @@
 #pragma once
+
+#include <SFML/Graphics.hpp>
+#include <entt/entt.hpp>
+
+class ShootingSystem
+{
+public:
+    static void shoot(entt::registry& registry, sf::Time deltaTime);
+};
