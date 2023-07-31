c5a398ae19f6403bcb90df4fbe9d016051a9a665
Aleks <aleks.koloch@gmail.com>
2023-07-31 17:26:58 +0200

    Update MathOperations functions to inline

--- src/utils/MathOperations.h
+++ src/utils/MathOperations.h
@@ -11,17 +11,17 @@ inline float lerp(float a, float b, float f)
     return a + f * (b - a);
 }
 
-float DistanceToMouse(const sf::Vector2f& mousePosition, const Position& position)
+inline float DistanceToMouse(const sf::Vector2f& mousePosition, const Position& position)
 {
     return std::sqrt(std::pow(mousePosition.x - position.position.x, 2) + std::pow(mousePosition.y - position.position.y, 2));
 }
 
-float TargetAngle(const sf::Vector2f& mousePosition, const Position& position, float offset = 90.0f)
+inline float TargetAngle(const sf::Vector2f& mousePosition, const Position& position, float offset = 90.0f)
 {
     return std::atan2(mousePosition.y - position.position.y, mousePosition.x - position.position.x) * (180 / M_PI) + offset;
 }
 
-float AngleDifference(float targetAngle, float currentAngle)
+inline float AngleDifference(float targetAngle, float currentAngle)
 {
     float angleDifference = targetAngle - currentAngle;
     if (angleDifference > 180.0f)
