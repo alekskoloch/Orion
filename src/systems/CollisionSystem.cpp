9e06d264b708e59ac934bd43927cdc8a9f8a2406
Aleks <aleks.koloch@gmail.com>
2023-08-03 21:00:13 +0200

    Add CollisionSystem implementation

--- src/systems/CollisionSystem.cpp
+++ src/systems/CollisionSystem.cpp
@@ -1,1 +1,38 @@
 #include "CollisionSystem.h"
+
+#include "../components/components.h"
+#include "../components/tagComponents.h"
+
+void CollisionSystem::updateCollisionBoxes(entt::registry& registry)
+{
+    auto view = registry.view<Collision, Renderable>();
+    for (auto entity : view)
+    {
+        auto& collision = view.get<Collision>(entity);
+        auto& renderable = view.get<Renderable>(entity);
+
+        collision.collisionBox = renderable.sprite.getGlobalBounds();
+    }
+}
+
+void CollisionSystem::checkCollisions(entt::registry& registry)
+{
+    auto bullets = registry.view<Collision, Renderable, Velocity, Bullet>();
+    auto enemies = registry.view<Collision, Enemy>();
+
+    for (auto bullet : bullets)
+    {
+        auto& bulletCollision = bullets.get<Collision>(bullet);
+
+        for (auto enemy : enemies)
+        {
+            auto& enemyCollision = enemies.get<Collision>(enemy);
+
+            if (bulletCollision.collisionBox.intersects(enemyCollision.collisionBox))
+            {
+                registry.destroy(bullet);
+                registry.destroy(enemy);
+            }
+        }
+    }
+}
--- src/systems/CollisionSystem.h
+++ src/systems/CollisionSystem.h
@@ -1,1 +1,10 @@
 #pragma once
+
+#include <entt/entt.hpp>
+
+class CollisionSystem
+{
+public:
+    static void updateCollisionBoxes(entt::registry& registry);
+    static void checkCollisions(entt::registry& registry);
+};
