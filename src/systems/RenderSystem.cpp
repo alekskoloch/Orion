4b57f5316afc8062b8725facebb9a51e41a538b1
Aleks <aleks.koloch@gmail.com>
2023-08-06 14:52:53 +0200

    Add display collision boxes for debugging

--- src/systems/RenderSystem.cpp
+++ src/systems/RenderSystem.cpp
@@ -11,4 +11,23 @@ void RenderSystem::renderEntities(sf::RenderWindow& window, entt::registry& regi
         renderable.sprite.setPosition(position.position);
         window.draw(renderable.sprite);
     }
+
+    //TODO: Make this a separate debug system
+    auto collisionView = registry.view<Collision>();
+    for (auto entity : collisionView)
+    {
+        const auto& collision = collisionView.get<Collision>(entity);
+
+        sf::RectangleShape rectBox;
+
+        rectBox.setSize(sf::Vector2f(collision.collisionBox.width, collision.collisionBox.height));
+
+        rectBox.setPosition(collision.collisionBox.left, collision.collisionBox.top);
+
+        rectBox.setFillColor(sf::Color::Transparent);
+        rectBox.setOutlineColor(sf::Color::White);
+        rectBox.setOutlineThickness(-1.0f);
+
+        window.draw(rectBox);
+    }
 }
