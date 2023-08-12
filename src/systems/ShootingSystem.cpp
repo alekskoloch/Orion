d58f0d882c682b0a00f5084f664600f05bda46f6
Aleks <aleks.koloch@gmail.com>
2023-08-12 14:49:07 +0200

    Refactor create bullet function

--- src/systems/ShootingSystem.cpp
+++ src/systems/ShootingSystem.cpp
@@ -4,6 +4,20 @@
 #include "../components/components.h"
 #include "../components/tagComponents.h"
 
+template <typename BulletOwnerTag>
+void createBullet(entt::registry& registry, sf::Vector2f position, sf::FloatRect& hitbox, sf::Sprite& sprite, sf::Vector2f velocity)
+{
+    auto bulletEntity = registry.create();
+    registry.emplace<Bullet>(bulletEntity);
+    registry.emplace<BulletOwnerTag>(bulletEntity);
+
+    registry.emplace<Position>(bulletEntity, position);
+    registry.emplace<Collision>(bulletEntity, hitbox);
+
+    registry.emplace<Velocity>(bulletEntity, velocity);
+    registry.emplace<Renderable>(bulletEntity, sprite);
+}
+
 void ShootingSystem::shoot(entt::registry& registry, sf::Time deltaTime)
 {
     auto view = registry.view<Weapon, Input, Position, Renderable>();
@@ -37,28 +51,19 @@ void ShootingSystem::shoot(entt::registry& registry, sf::Time deltaTime)
             //TODO: Refactor this
             if (weapon.weaponType == WeaponType::SingleShot)
             {
-                auto bulletEntity = registry.create();
-                registry.emplace<Bullet>(bulletEntity);
-                registry.emplace<PlayerBullet>(bulletEntity);
-
                 sf::Sprite sprite(TextureManager::getInstance().getTexture(weapon.bulletTextureName));
                 sprite.setOrigin(sprite.getGlobalBounds().width / 2.f, sprite.getGlobalBounds().height / 2.f);
 
-                registry.emplace<Position>(bulletEntity, playerPosition.position);
-
                 sf::FloatRect collisionRect(playerPosition.position.x - 3.5f, playerPosition.position.y - 3.5f, 7.f, 7.f);
-
-                registry.emplace<Collision>(bulletEntity, collisionRect);
 
                 float rotation = playerRenderable.sprite.getRotation();
                 sprite.setRotation(rotation);
-
                 float adjustedRotation = 90.f - rotation;
                 float radians = adjustedRotation * (3.14159265f / 180.f);
+
                 sf::Vector2f velocity(std::cos(radians) * weapon.bulletSpeed, -std::sin(radians) * weapon.bulletSpeed);
 
-                registry.emplace<Velocity>(bulletEntity, velocity);
-                registry.emplace<Renderable>(bulletEntity, sprite);
+                createBullet<PlayerBullet>(registry, playerPosition.position, collisionRect, sprite, velocity);
 
                 weapon.currentCooldownTime = weapon.cooldownTime;
             }
@@ -68,28 +73,18 @@ void ShootingSystem::shoot(entt::registry& registry, sf::Time deltaTime)
 
                 for (float offset : angleOffset)
                 {
-                    auto bulletEntity = registry.create();
-                    registry.emplace<Bullet>(bulletEntity);
-                    registry.emplace<PlayerBullet>(bulletEntity);
-
                     sf::Sprite sprite(TextureManager::getInstance().getTexture(weapon.bulletTextureName));
                     sprite.setOrigin(sprite.getGlobalBounds().width / 2.f, sprite.getGlobalBounds().height / 2.f);
 
-                    registry.emplace<Position>(bulletEntity, playerPosition.position);
-
                     sf::FloatRect collisionRect(playerPosition.position.x - 3.5f, playerPosition.position.y - 3.5f, 7.f, 7.f);
-                    
-                    registry.emplace<Collision>(bulletEntity, collisionRect);
 
                     float rotation = playerRenderable.sprite.getRotation() + offset;
                     sprite.setRotation(rotation);
-
                     float adjustedRotation = 90.f - rotation;
                     float radians = adjustedRotation * (3.14159265f / 180.f);
                     sf::Vector2f velocity(std::cos(radians) * weapon.bulletSpeed, -std::sin(radians) * weapon.bulletSpeed);
 
-                    registry.emplace<Velocity>(bulletEntity, velocity);
-                    registry.emplace<Renderable>(bulletEntity, sprite);
+                    createBullet<PlayerBullet>(registry, playerPosition.position, collisionRect, sprite, velocity);
                 }
 
                 weapon.currentCooldownTime = weapon.cooldownTime;
@@ -123,23 +118,27 @@ void ShootingSystem::shoot(entt::registry& registry, sf::Time deltaTime)
                 float length = std::sqrt(direction.x * direction.x + direction.y * direction.y);
                 sf::Vector2f normalizedDirection = direction / length;
 
-                auto bulletEntity = registry.create();
-                registry.emplace<Bullet>(bulletEntity);
-                registry.emplace<EnemyBullet>(bulletEntity);
+                // auto bulletEntity = registry.create();
+                // registry.emplace<Bullet>(bulletEntity);
+                // registry.emplace<EnemyBullet>(bulletEntity);
 
                 sf::Sprite sprite(TextureManager::getInstance().getTexture(enemyWeapon.bulletTextureName));
                 sprite.setOrigin(sprite.getGlobalBounds().width / 2.f, sprite.getGlobalBounds().height / 2.f);
 
+                sf::FloatRect collisionRect(sprite.getGlobalBounds());
+
                 float rotation = std::atan2(normalizedDirection.y, normalizedDirection.x) * (180.f / 3.14159265f) + 90.f;
                 sprite.setRotation(rotation);
 
-                registry.emplace<Position>(bulletEntity, enemyPosition.position);
-                registry.emplace<Collision>(bulletEntity, sprite.getGlobalBounds());
+                // registry.emplace<Position>(bulletEntity, enemyPosition.position);
+                // registry.emplace<Collision>(bulletEntity, sprite.getGlobalBounds());
 
                 sf::Vector2f velocity = normalizedDirection * enemyWeapon.bulletSpeed;
 
-                registry.emplace<Velocity>(bulletEntity, velocity);
-                registry.emplace<Renderable>(bulletEntity, sprite);
+                // registry.emplace<Velocity>(bulletEntity, velocity);
+                // registry.emplace<Renderable>(bulletEntity, sprite);
+
+                createBullet<EnemyBullet>(registry, enemyPosition.position, collisionRect, sprite, velocity);
 
                 enemyWeapon.currentCooldownTime = enemyWeapon.cooldownTime;
             }
