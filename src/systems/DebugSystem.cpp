232c61fa5b7694f6632df4cab60ed680ccc4a5d1
Aleks <aleks.koloch@gmail.com>
2023-08-13 13:32:59 +0200

    Add rendetCollisionBoxes function to DebugSystem

--- src/systems/DebugSystem.cpp
+++ src/systems/DebugSystem.cpp
@@ -1,1 +1,25 @@
 #include "DebugSystem.h"
+
+#include "../components/components.h"
+
+void DebugSystem::renderCollisionBoxes(entt::registry& registry, sf::RenderWindow& window)
+{
+    auto collisionView = registry.view<Collision>();
+    for (auto entity : collisionView)
+    {
+        const auto& collision = collisionView.get<Collision>(entity);
+
+        sf::RectangleShape rectBox;
+
+        rectBox.setSize(sf::Vector2f(collision.collisionBox.width, collision.collisionBox.height));
+
+        rectBox.setPosition(collision.collisionBox.left, collision.collisionBox.top);
+
+        rectBox.setFillColor(sf::Color::Transparent);
+        rectBox.setOutlineColor(sf::Color::White);
+        rectBox.setOutlineThickness(-1.0f);
+
+        window.draw(rectBox);
+    }
+}
+    
--- src/systems/DebugSystem.h
+++ src/systems/DebugSystem.h
@@ -1,3 +1,9 @@
 #pragma once
 
 #include "../pch.h"
+
+class DebugSystem
+{
+public:
+    static void renderCollisionBoxes(entt::registry& registry, sf::RenderWindow& window);
+};
