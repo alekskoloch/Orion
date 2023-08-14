430142ccd0a15ae79e0f872ad33629bf6bd44531
Aleks <aleks.koloch@gmail.com>
2023-08-14 14:46:56 +0200

    Add random header to pch

--- src/pch.h
+++ src/pch.h
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <unordered_map>
 #include <string>
+#include <random>
 
 #include <SFML/Graphics.hpp>
 #include <entt/entt.hpp>
--- src/systems/ProceduralGenerationSystem.h
+++ src/systems/ProceduralGenerationSystem.h
@@ -1,3 +1,15 @@
 #pragma once
 
 #include "../pch.h"
+
+class ProceduralGenerationSystem
+{
+public:
+    //initialize procedural generator system with seed as parameter
+    static void Initialize(int seed);
+    //get number of any range
+    static int GetRandomNumber(int min, int max);
+private:
+    //private static variables for procedural generator
+    
+};
