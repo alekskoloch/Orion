b88da3017150ea878ce58db2de341fe58d5b83e3
Aleks <aleks.koloch@gmail.com>
2023-08-16 16:07:53 +0200

    Add GUIEnergyBar simple implementation

--- src/managers/GUIElements/GUIEnergyBar.cpp
+++ src/managers/GUIElements/GUIEnergyBar.cpp
@@ -1,1 +1,41 @@
 #include "GUIEnergyBar.h"
+
+#include "../../components/components.h"
+#include "../../components/tagComponents.h"
+
+GUIEnergyBar::GUIEnergyBar(sf::RenderWindow& window, entt::registry& registry)
+    : window(window), registry(registry)
+{
+    this->initializeEnergyBar();
+}
+
+void GUIEnergyBar::update()
+{
+    auto playerEnergyView = this->registry.view<Player, Energy>();
+    for (auto player : playerEnergyView)
+    {
+        auto& playerEnergy = playerEnergyView.get<Energy>(player);
+
+        this->energyBarSprite.setSize(sf::Vector2f((playerEnergy.currentEnergyValue / playerEnergy.maxEnergyValue) * 300, 50));
+    }
+}
+
+void GUIEnergyBar::draw()
+{
+    this->window.draw(this->energyBarSprite);
+    this->window.draw(this->energyBarBackgroundSprite);
+}
+
+void GUIEnergyBar::initializeEnergyBar()
+{
+    //TODO: Temporary solution
+    this->energyBarSprite.setSize(sf::Vector2f(300, 50));
+    this->energyBarSprite.setFillColor(sf::Color::Blue);
+    this->energyBarSprite.setPosition(3300, 2000);
+
+    this->energyBarBackgroundSprite.setSize(sf::Vector2f(300, 50));
+    this->energyBarBackgroundSprite.setFillColor(sf::Color::Transparent);
+    this->energyBarBackgroundSprite.setOutlineColor(sf::Color::White);
+    this->energyBarBackgroundSprite.setOutlineThickness(-5.0f);
+    this->energyBarBackgroundSprite.setPosition(3300, 2000);
+}
--- src/managers/GUIElements/GUIEnergyBar.h
+++ src/managers/GUIElements/GUIEnergyBar.h
@@ -4,5 +4,18 @@
 
 class GUIEnergyBar
 {
+public:
+    GUIEnergyBar(sf::RenderWindow& window, entt::registry& registry);
 
+    void update();
+    void draw();
+
+private:
+    sf::RenderWindow& window;
+    entt::registry& registry;
+
+    sf::RectangleShape energyBarSprite;
+    sf::RectangleShape energyBarBackgroundSprite;
+
+    void initializeEnergyBar();
 };
