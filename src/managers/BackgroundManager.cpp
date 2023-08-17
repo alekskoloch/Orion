5af3b0b971fccef8112fe07c115db0640efa0eb9
Aleks <aleks.koloch@gmail.com>
2023-08-17 12:50:54 +0200

    Add basic implementation BackgroundManager for testing

--- src/managers/BackgroundManager.cpp
+++ src/managers/BackgroundManager.cpp
@@ -1,1 +1,64 @@
 #include "BackgroundManager.h"
+
+#include "../components/components.h"
+#include "../components/tagComponents.h"
+
+//TODO: Color for tiles is only for testing purposes
+
+BackgroundManager::BackgroundManager(entt::registry& registry, sf::RenderWindow& window) : registry(registry), window(window)
+{
+    for (int x = -tilesAroundPlayer; x < tilesAroundPlayer+1; x++)
+    {
+        for (int y = -tilesAroundPlayer; y < tilesAroundPlayer+1; y++)
+        {
+            backgroundTiles.push_back(BackgroundTile(
+                sf::Vector2f(0.f + x * backgroundTileSize, 0.f + y * backgroundTileSize),
+                sf::Vector2f(backgroundTileSize, backgroundTileSize),
+                sf::Color(100 + (x * 10), 100 + (y * 10), 100)
+            ));
+        }
+    }
+}
+
+void BackgroundManager::update()
+{
+    auto playerView = registry.view<Player, Position>();
+    auto playerEntity = playerView.front();
+    auto& playerPosition = playerView.get<Position>(playerEntity);
+
+    int playerTileX = static_cast<int>(playerPosition.position.x / backgroundTileSize);
+    int playerTileY = static_cast<int>(playerPosition.position.y / backgroundTileSize);
+
+    if (playerTileX != currentPlayerTileX || playerTileY != currentPlayerTileY)
+    {
+        currentPlayerTileX = playerTileX;
+        currentPlayerTileY = playerTileY;
+
+        updateBackgroundTiles(playerTileX, playerTileY);
+    }
+}
+
+void BackgroundManager::draw()
+{
+    for (auto& backgroundTile : backgroundTiles)
+    {
+        backgroundTile.draw(window);
+    }
+}
+
+void BackgroundManager::updateBackgroundTiles(int playerTileX, int playerTileY)
+{
+    backgroundTiles.clear();
+
+    for (int x = playerTileX - tilesAroundPlayer; x <= playerTileX + tilesAroundPlayer; x++)
+    {
+        for (int y = playerTileY - tilesAroundPlayer; y <= playerTileY + tilesAroundPlayer; y++)
+        {
+            backgroundTiles.push_back(BackgroundTile(
+                sf::Vector2f(x * backgroundTileSize, y * backgroundTileSize),
+                sf::Vector2f(backgroundTileSize, backgroundTileSize),
+                sf::Color(100 + (x * 10), 100 + (y * 10), 100)
+            ));
+        }
+    }
+}
--- src/managers/BackgroundManager.h
+++ src/managers/BackgroundManager.h
@@ -2,7 +2,52 @@
 
 #include "../pch.h"
 
+class BackgroundTile
+{
+public:
+    BackgroundTile(sf::Vector2f position, sf::Vector2f size, sf::Color color)
+    {
+        this->position = position;
+        this->size = size;
+
+        tile.setSize(size);
+        tile.setPosition(position);
+        tile.setFillColor(color);
+        
+        tile.setOrigin(size.x / 2, size.y / 2);
+    }
+
+    void draw(sf::RenderWindow& window)
+    {
+        window.draw(tile);
+    }
+
+private:
+    sf::Vector2f position;
+    sf::Vector2f size;
+
+    sf::RectangleShape tile;
+};
+
 class BackgroundManager
 {
+public:
+    BackgroundManager(entt::registry& registry, sf::RenderWindow& window);
 
+    void update();
+    void draw();
+    
+private:
+    entt::registry& registry;
+    sf::RenderWindow& window;
+
+    int currentPlayerTileX = 0;
+    int currentPlayerTileY = 0;
+
+    int tilesAroundPlayer = 6;
+
+    float backgroundTileSize = 400.f;
+    std::vector<BackgroundTile> backgroundTiles;
+
+    void updateBackgroundTiles(int playerTileX, int playerTileY);
 };
