8b0539222695949c49da8adcb84822c16b333534
Aleks <aleks.koloch@gmail.com>
2023-09-06 23:10:06 +0200

    Add ShieldSchema

---
+++ src/schema/ShieldSchema.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "../pch.h"
+
+struct ShieldSchema
+{
+    float durability;
+    float energyCost;
+    float duration;
+
+    std::string shieldTextureName;
+    std::string shieldIconTextureName;
+};
