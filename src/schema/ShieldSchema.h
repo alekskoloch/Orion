c52bbadf366e82fd297098f18bc2a154fbddacbf
Aleks <aleks.koloch@gmail.com>
2023-09-08 10:15:10 +0200

    Update shield schema to handle loadTime

--- src/schema/ShieldSchema.h
+++ src/schema/ShieldSchema.h
@@ -8,8 +8,11 @@ struct ShieldSchema
     float energyCost;
     float duration;
 
+    float loadTime;
+
     std::string shieldTextureName;
     std::string shieldIconTextureName;
+
 };
 
 static ShieldSchema basicShield =
@@ -17,6 +20,7 @@ static ShieldSchema basicShield =
     1.f,
     50.f,
     3.f,
+    2.f,
     "shield",
     "basic_shield_ico"
 };
@@ -26,6 +30,7 @@ static ShieldSchema advancedShield =
     1.f,
     100.f,
     10.f,
+    4.f,
     "shield_advanced",
     "advanced_shield_ico"
 };
