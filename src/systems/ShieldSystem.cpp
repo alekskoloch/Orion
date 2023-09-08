c8bb63231b2fb9c8ea44530372c1baa27c0b14f9
Aleks <aleks.koloch@gmail.com>
2023-09-08 12:02:54 +0200

    Update logic handles shield

--- src/systems/ShieldSystem.cpp
+++ src/systems/ShieldSystem.cpp
@@ -15,14 +15,20 @@ void ShieldSystem::loadShield(entt::registry& registry, const ShieldSchema& shie
 
 void ShieldSystem::changeShield(entt::registry& registry, ShieldSchema shield)
 {
-    auto player = registry.view<Player, Shield>();
+    auto player = registry.view<Player, Shield, Energy>();
     for (auto& entity : player)
     {
+        auto& energy = player.get<Energy>(entity);
         auto& shieldComponent = player.get<Shield>(entity);
         shieldComponent.durability = shield.durability;
         shieldComponent.energyCost = shield.energyCost;
         shieldComponent.duration = shield.duration;
         shieldComponent.loadTime = shield.loadTime;
+
+        energy.currentEnergyValue += shieldComponent.energyUsed;
+        shieldComponent.energyUsed = 0.f;
+        shieldComponent.currentLoadTime = 0.f;
+        shieldComponent.currentDuration = 0.f;
 
         TextureManager::getInstance().loadTexture(shield.shieldTextureName, ASSETS_PATH + shield.shieldTextureName + ".png");
         shieldComponent.shieldTextureName = shield.shieldTextureName;
@@ -33,27 +39,47 @@ void ShieldSystem::changeShield(entt::registry& registry, ShieldSchema shield)
 
 void ShieldSystem::updateShield(entt::registry& registry, sf::Time deltaTime)
 {
-    auto view = registry.view<Player, Shield, Input>();
+    auto view = registry.view<Player, Shield, Input, Energy>();
 
     for (auto entity : view)
     {
         auto& input = view.get<Input>(entity);
         auto& shield = view.get<Shield>(entity);
+        auto& energy = view.get<Energy>(entity);
 
         if (input.isGettingShield)
         {
             if (shield.currentLoadTime < shield.loadTime)
+            {
+                //TODO: Temporary energy regeneration management
+                energy.energyRegenerationRate = 0;
+                if (energy.currentEnergyValue > 0)
+                {
+                    shield.energyUsed += shield.energyCost * (deltaTime.asSeconds() / shield.loadTime);
+                    energy.currentEnergyValue -= shield.energyCost * (deltaTime.asSeconds() / shield.loadTime);
+                    if (energy.currentEnergyValue < 0)
+                        energy.currentEnergyValue = 0;
+                }
                 shield.currentLoadTime += deltaTime.asSeconds();
+            }
         }
         else if (shield.currentLoadTime != 0 && !input.getShield)
         {
+            energy.energyRegenerationRate = 10.f;
+            energy.currentEnergyValue += shield.energyUsed;
+            shield.energyUsed = 0.f;
             shield.currentLoadTime = 0;
         }
 
         if (shield.currentLoadTime >= shield.loadTime && input.getShield)
         {
+            energy.energyRegenerationRate = 10.f;
             shield.currentLoadTime = 0;
-            getShield(registry);
+            if (shield.energyUsed >= shield.energyCost)
+            {
+                getShield(registry);
+            }
+            shield.energyUsed = 0.f;
         }
 
         if (shield.active)
@@ -86,39 +112,34 @@ void ShieldSystem::updateShield(entt::registry& registry, sf::Time deltaTime)
 
 void ShieldSystem::getShield(entt::registry& registry)
 {
-    auto view = registry.view<Player, Shield, Energy>();
+    auto view = registry.view<Player, Shield>();
 
     for (auto entity : view)
     {
-        auto& energy = view.get<Energy>(entity);
         auto& shield = view.get<Shield>(entity);
         
-        if (energy.currentEnergyValue >= shield.energyCost)
+        if (shield.active)
         {
-            if (shield.active)
+            auto shieldView = registry.view<PlayerShield>();
+            for (auto entity : shieldView)
             {
-                auto shieldView = registry.view<PlayerShield>();
-                for (auto entity : shieldView)
-                {
-                    registry.destroy(entity);
-                }
+                registry.destroy(entity);
             }
-
-            energy.currentEnergyValue -= shield.energyCost;
-            shield.currentDuration = shield.duration;
-
-            shield.active = true;
-
-            //creating shield
-
-            auto shieldEntity = registry.create();
-            registry.emplace<PlayerShield>(shieldEntity);
-
-            sf::Sprite shieldSprite(TextureManager::getInstance().getTexture(shield.shieldTextureName));
-            shieldSprite.setOrigin(shieldSprite.getGlobalBounds().width / 2.f, shieldSprite.getGlobalBounds().height / 2.f);
-            registry.emplace<Renderable>(shieldEntity, shieldSprite);
-
-            registry.emplace<Position>(shieldEntity, registry.get<Position>(entity).position);
         }
+
+        shield.currentDuration = shield.duration;
+
+        shield.active = true;
+
+        //creating shield
+
+        auto shieldEntity = registry.create();
+        registry.emplace<PlayerShield>(shieldEntity);
+
+        sf::Sprite shieldSprite(TextureManager::getInstance().getTexture(shield.shieldTextureName));
+        shieldSprite.setOrigin(shieldSprite.getGlobalBounds().width / 2.f, shieldSprite.getGlobalBounds().height / 2.f);
+        registry.emplace<Renderable>(shieldEntity, shieldSprite);
+
+        registry.emplace<Position>(shieldEntity, registry.get<Position>(entity).position);
     }
 }
