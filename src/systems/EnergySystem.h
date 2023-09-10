92693f15ea59cf8a3457480f2c5a5c2991919ea3
Aleks <aleks.koloch@gmail.com>
2023-09-10 20:34:26 +0200

    Add hasAnyEnergy function in EnergySystem

--- src/systems/EnergySystem.h
+++ src/systems/EnergySystem.h
@@ -33,4 +33,20 @@ public:
             energyComponent.regeneration = false;
         }
     }
+
+    template<typename EnergyOwnerTag>
+    requires UniqueTagConcept<EnergyOwnerTag>
+    static bool hasAnyEnergy(entt::registry& registry)
+    {
+        auto view = registry.view<Energy, EnergyOwnerTag>();
+
+        for (auto entity : view)
+        {
+            auto& energyComponent = view.template get<Energy>(entity);
+            if (energyComponent.currentEnergyValue > 0)
+                return true;
+        }
+
+        return false;
+    }
 };
