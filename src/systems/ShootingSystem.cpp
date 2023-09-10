4edaf29c0b1dea8df7ba5fc48ebe703b59d4d1aa
Aleks <aleks.koloch@gmail.com>
2023-09-10 11:19:54 +0200

    Style code in createBullet function

--- src/systems/ShootingSystem.cpp
+++ src/systems/ShootingSystem.cpp
@@ -13,23 +13,21 @@ template <typename BulletOwnerTag>
 void createBullet(entt::registry& registry, entt::entity& entity, sf::Vector2f targetPosition, float offset = 0.f)
 {
     auto weapon = registry.get<Weapon>(entity);
-    auto position = registry.get<Position>(entity);
+    auto position = registry.get<Position>(entity).position;
 
-    sf::Vector2f direction(CalculateDirectionBetweenPoints(position.position, targetPosition, offset));
+    sf::Vector2f direction(CalculateDirectionBetweenPoints(position, targetPosition, offset));
     float rotation = CalculateAzimuthAngleInDegrees(direction, 90.f);
 
     sf::Sprite sprite = CreateSprite(weapon.bulletTextureName);
-
     sprite.setRotation(rotation);
 
     auto bulletEntity = registry.create();
     registry.emplace<Bullet>(bulletEntity);
     registry.emplace<BulletOwnerTag>(bulletEntity);
 
-    registry.emplace<Position>(bulletEntity, position.position);
+    registry.emplace<Position>(bulletEntity, position);
     //TODO: Get Collision from weaponSchema
     registry.emplace<Collision>(bulletEntity, sprite.getGlobalBounds());
-
     registry.emplace<Velocity>(bulletEntity, direction * weapon.bulletSpeed);
     registry.emplace<Renderable>(bulletEntity, sprite);
 }
