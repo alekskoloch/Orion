22091ff16c80494e097ff9b0523a5160eb369155
Aleks <aleks.koloch@gmail.com>
2023-09-14 18:23:04 +0200

    Add SceneManager implementation

--- src/managers/SceneManager.cpp
+++ src/managers/SceneManager.cpp
@@ -1,1 +1,17 @@
 #include "SceneManager.h"
+
+SceneManager& SceneManager::getInstance()
+{
+    static SceneManager instance;
+    return instance;
+}
+
+void SceneManager::setCurrentScene(Scene scene)
+{
+    this->currentScene = scene;
+}
+
+Scene SceneManager::getCurrentScene()
+{
+    return this->currentScene;
+}
--- src/managers/SceneManager.h
+++ src/managers/SceneManager.h
@@ -2,7 +2,25 @@
 
 #include "../pch.h"
 
+enum class Scene
+{
+    MainMenu,
+    Game,
+    SkillTree,
+    GameOver
+};
+
 class SceneManager
 {
+private:
+    SceneManager() = default;
+    Scene currentScene;
+public:
+    static SceneManager& getInstance();
 
+    SceneManager(SceneManager const&) = delete;
+    void operator=(SceneManager const&) = delete;
+
+    void setCurrentScene(Scene scene);
+    Scene getCurrentScene();
 };
