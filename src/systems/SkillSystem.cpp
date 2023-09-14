f664fee83225ad488ef0db96ad8361293d757bd7
Aleks <aleks.koloch@gmail.com>
2023-09-14 20:22:08 +0200

    Add updateWeaponDamageMultiplier function

--- src/systems/SkillSystem.cpp
+++ src/systems/SkillSystem.cpp
@@ -1,1 +1,10 @@
 #include "SkillSystem.h"
+
+#include "../components/components.h"
+#include "../components/tagComponents.h"
+
+void SkillSystem::updateWeaponDamageMultiplier(entt::registry& registry, float multiplier)
+{
+    auto& playerSkillsComponent = registry.get<Skills>(registry.view<Player>().front());
+    playerSkillsComponent.weaponDamageMultiplier += multiplier;
+}
--- src/systems/SkillSystem.h
+++ src/systems/SkillSystem.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include "../pch.h"
+
 class SkillSystem
 {
-
+    static void updateWeaponDamageMultiplier(entt::registry& registry, float multiplier);
 };
