3a4706c118bde49f83675db459978c5f57aa41b1
Aleks <aleks.koloch@gmail.com>
2023-09-16 00:20:12 +0200

    Refactor handling skills to SkillManager

--- src/managers/GUIScenes/GUISkillTree.h
+++ src/managers/GUIScenes/GUISkillTree.h
@@ -4,41 +4,7 @@
 
 #include "../../schema/SkillSchema.h"
 
-class Skill
-{
-public:
-    Skill(sf::RenderWindow& window, entt::registry& registry, sf::Font& font, sf::Vector2f iconPosition, std::string name, std::string description, std::string iconTextureName, std::string iconHoverTextureName, std::string iconActiveTextureName, std::function<void(entt::registry&)> onActivate);
-
-    void update();
-    void draw();
-private:
-    sf::RenderWindow& window;
-    entt::registry& registry;
-
-    void initialize();
-
-    sf::Sprite iconSprite;
-    sf::Vector2f iconPosition;
-
-    std::string iconTextureName;
-    std::string iconHoverTextureName;
-    std::string iconActiveTextureName;
-
-    void initializeText();
-    void updateText();
-
-    std::string name;
-    std::string description;
-    
-    sf::Font& font;
-    sf::Text nameText;
-    sf::Text descriptionText;
-
-    bool active = false;
-    bool hover = false;
-
-    std::function<void(entt::registry&)> onActivate;
-};
+#include "../SkillManager.h"
 
 class GUISkillTree
 {
@@ -53,10 +19,8 @@ private:
 
     sf::Font font;
 
+    SkillManager skillManager;
+
     sf::Sprite skillBackground;
     void initializeSkillBackground();
-
-    std::vector<Skill> skills;
-    void initializeSkills();
-    void addSkill(const SkillSchema& skill);
 };
--- src/managers/SkillManager.h
+++ src/managers/SkillManager.h
@@ -1,1 +1,59 @@
 #pragma once
+
+#include "../pch.h"
+
+#include "../schema/SkillSchema.h"
+#include "../components/components.h"
+
+class Skill
+{
+public:
+    Skill(sf::RenderWindow& window, entt::registry& registry, sf::Font& font, sf::Vector2f iconPosition, std::string name, std::string description, std::string iconTextureName, std::string iconHoverTextureName, std::string iconActiveTextureName, std::function<void(entt::registry&)> onActivate);
+
+    void update();
+    void draw();
+private:
+    sf::RenderWindow& window;
+    entt::registry& registry;
+
+    void initialize();
+
+    sf::Sprite iconSprite;
+    sf::Vector2f iconPosition;
+
+    std::string iconTextureName;
+    std::string iconHoverTextureName;
+    std::string iconActiveTextureName;
+
+    void initializeText();
+    void updateText();
+
+    std::string name;
+    std::string description;
+    
+    sf::Font& font;
+    sf::Text nameText;
+    sf::Text descriptionText;
+
+    bool active = false;
+    bool hover = false;
+
+    std::function<void(entt::registry&)> onActivate;
+};
+
+class SkillManager
+{
+public:
+    SkillManager(sf::RenderWindow& window, entt::registry& registry);
+
+    void update();
+    void draw();
+private:
+    sf::RenderWindow& window;
+    entt::registry& registry;
+    std::vector<Skill> skills;
+    sf::Font font;
+
+    void addSkill(const SkillSchema& skill);
+    void initializeFirstSkill();
+};
