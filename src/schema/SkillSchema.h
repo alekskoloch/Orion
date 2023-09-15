a07bcf5663e16d8e2767bf6c59aff8c7f0353e53
Aleks <aleks.koloch@gmail.com>
2023-09-15 18:54:47 +0200

    Add SkillSchema struct

--- src/schema/SkillSchema.h
+++ src/schema/SkillSchema.h
@@ -1,1 +1,14 @@
 #pragma once
+
+#include "../pch.h"
+
+struct SkillSchema
+{
+    sf::Vector2f position;
+    std::string name;
+    std::string description;
+    std::string textureName;
+    std::string hoverTextureName;
+    std::string activeTextureName;
+    std::function<void(entt::registry& registry)> callback;
+};
