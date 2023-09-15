eae3fe6369e9e2b362bcdd14246a723a2c770273
Aleks <aleks.koloch@gmail.com>
2023-09-15 18:59:15 +0200

    Add OrionProtocol skill schema

--- src/schema/SkillSchema.h
+++ src/schema/SkillSchema.h
@@ -2,6 +2,9 @@
 
 #include "../pch.h"
 
+#include "../components/components.h"
+#include "../components/tagComponents.h"
+
 struct SkillSchema
 {
     sf::Vector2f position;
@@ -11,4 +14,19 @@ struct SkillSchema
     std::string hoverTextureName;
     std::string activeTextureName;
     std::function<void(entt::registry& registry)> callback;
+};
+
+static SkillSchema OrionProtocol
+{
+    sf::Vector2f(1920.f, 1080.f),
+    "Orion protocol",
+    "All bullets deal damage increased by 5%",
+    "skill",
+    "skillHover",
+    "skillActive",
+    [](entt::registry& registry)
+    {
+        auto& playerSkillsComponent = registry.get<Skills>(registry.view<Player>().front());
+        playerSkillsComponent.weaponDamageMultiplier += 0.05f;
+    }
 };
