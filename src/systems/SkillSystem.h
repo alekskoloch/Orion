bd0334676c7c337fc7aa04484bb92fffada354e8
Aleks <aleks.koloch@gmail.com>
2023-09-15 18:41:05 +0200

    Add public access specifier to SkillSystem

--- src/systems/SkillSystem.h
+++ src/systems/SkillSystem.h
@@ -4,6 +4,7 @@
 
 class SkillSystem
 {
+public:
     static void updateWeaponDamageMultiplier(entt::registry& registry, float multiplier);
     static float getWeaponDamage(entt::registry& registry, entt::entity entity); 
 };
