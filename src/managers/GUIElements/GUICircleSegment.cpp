519211e0f1b356dda1ed723ee45716dc0d8d86c2
Aleks <aleks.koloch@gmail.com>
2023-09-22 15:23:29 +0200

    Add GUICircleSegment implementation

--- src/managers/GUIElements/GUICircleSegment.cpp
+++ src/managers/GUIElements/GUICircleSegment.cpp
@@ -1,1 +1,75 @@
 #include "GUICircleSegment.h"
+
+void GUICircleSegment::update(float deltaTime)
+{
+    if (!this->active)
+    {
+        if (this->state == CircleSegmentState::Hover)
+        {
+            if (currentAngle < endAngle)
+            {
+                currentAngle += 1000.0f * deltaTime;
+                if (currentAngle > endAngle)
+                    currentAngle = endAngle;
+            }
+
+            this->createCircleSegment();
+        }
+        else
+        {
+            if (currentAngle > startAngle)
+            {
+                currentAngle -= 1000.0f * deltaTime;
+                if (currentAngle < startAngle)
+                    currentAngle = startAngle;
+            }
+            this->createCircleSegment();
+        }
+
+        if (this->state == CircleSegmentState::Active)
+        {
+            this->active = true;
+            color2 = color1;
+            currentAngle = endAngle;
+            this->createCircleSegment();
+        }
+    }
+}
+
+sf::Color GUICircleSegment::interpolateColor(sf::Color color1, sf::Color color2, float t)
+{
+    sf::Color color;
+    color.r = static_cast<sf::Uint8>((1 - t) * color1.r + t * color2.r);
+    color.g = static_cast<sf::Uint8>((1 - t) * color1.g + t * color2.g);
+    color.b = static_cast<sf::Uint8>((1 - t) * color1.b + t * color2.b);
+    color.a = static_cast<sf::Uint8>((1 - t) * color1.a + t * color2.a);
+    
+    return color;
+}
+
+void GUICircleSegment::createCircleSegment()
+{
+    this->segmentVertices.clear();
+
+    int numSegments = static_cast<int>(std::ceil((currentAngle - startAngle)));
+
+    float angleStep = (currentAngle - startAngle) / numSegments;
+    
+
+    for (int i = 0; i < numSegments; i++)
+    {
+        float angle1 = startAngle + i * angleStep;
+        float angle2 = startAngle + (i + 1) * angleStep;
+        sf::Color color = interpolateColor(color1, color2, static_cast<float>(i) / (numSegments - 1));
+
+        for (float t = angle1; t <= angle2; t += 0.01f)
+        {
+            float angle = t * M_PI / 180.0f;
+            sf::Vector2f outerPoint = center + sf::Vector2f(std::cos(angle), std::sin(angle)) * radius;
+            sf::Vector2f innerPoint = center + sf::Vector2f(std::cos(angle), std::sin(angle)) * (radius - thickness);
+
+            segmentVertices.emplace_back(outerPoint, color);
+            segmentVertices.emplace_back(innerPoint, color);
+        }
+    }
+}
--- src/managers/GUIElements/GUICircleSegment.h
+++ src/managers/GUIElements/GUICircleSegment.h
@@ -2,7 +2,47 @@
 
 #include "../../pch.h"
 
+enum class CircleSegmentState
+{
+    Hidden,
+    Hover,
+    Active
+};
+
 class GUICircleSegment : public sf::Drawable
 {
+public:
+    GUICircleSegment(sf::Vector2f center, float radius, float startAngle, float endAngle, float thickness, sf::Color color)
+        : center(center), radius(radius), startAngle(startAngle), endAngle(endAngle), thickness(thickness), color1(color)
+    {
+        this->createCircleSegment();
+    }
 
+    void update(float deltaTime);
+
+    void setState(CircleSegmentState state) { this->state = state; }
+
+private:
+    sf::Vector2f center;
+    float radius;
+    float thickness;
+
+    float startAngle;
+    float currentAngle = 0.0f;
+    float endAngle;
+
+    std::vector<sf::Vertex> segmentVertices;
+    sf::Color color1;
+    sf::Color color2 = sf::Color::Transparent;
+
+    CircleSegmentState state = CircleSegmentState::Hidden;
+    bool active = false;
+
+    void createCircleSegment();
+    sf::Color interpolateColor(sf::Color color1, sf::Color color2, float t);
+
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override
+    {
+        target.draw(segmentVertices.data(), segmentVertices.size(), sf::LinesStrip, states);
+    }
 };
