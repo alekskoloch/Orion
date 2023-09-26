18849767f5e04572f45291a21e7380716022ca53
Aleks <aleks.koloch@gmail.com>
2023-09-26 19:58:04 +0200

    Update RemovalSystem with functions to destroy enemy and bullets

--- src/systems/RemovalSystem.h
+++ src/systems/RemovalSystem.h
@@ -6,4 +6,13 @@ class RemovalSystem
 {
 public:
     static void update(entt::registry& registry);
+
+    static void MarkEntityForDestruction(entt::entity& entity);
+private:
+    static std::vector<entt::entity> entitiesToDestroy;
+    static int framesSinceLastExecution;
+
+    static void checkBulletRemoval(entt::registry& registry);
+    static void checkEnemyRemoval(entt::registry& registry);
+    static void destroyEntities(entt::registry& registry);
 };
