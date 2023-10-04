5dae66019d24f7501a86bc6952b4e0a220b4b350
Aleks <aleks.koloch@gmail.com>
2023-10-04 16:47:34 +0200

    Reafctor updateShurikenBullet by moving declaration to cpp file

--- src/systems/BulletSystem.cpp
+++ src/systems/BulletSystem.cpp
@@ -1,1 +1,33 @@
 #include "BulletSystem.h"
+
+void BulletSystem::updateShurikenBullet(entt::registry& registry, sf::Time deltaTime)
+{
+    auto shurikenBulletsView = registry.view<ShurikenBullet, Position>();
+
+    for (auto shurikenBulletEntity : shurikenBulletsView)
+    {
+        auto& shurikenBulletData = shurikenBulletsView.get<ShurikenBullet>(shurikenBulletEntity);
+        auto& position = shurikenBulletsView.get<Position>(shurikenBulletEntity);
+        
+        int multiplier = shurikenBulletData.right ? 1 : -1;
+
+        float x1 = shurikenBulletData.enemyPositionWhenShot.x;
+        float y1 = shurikenBulletData.enemyPositionWhenShot.y;
+
+        float x2 = shurikenBulletData.playerPositionWhenShot.x;
+        float y2 = shurikenBulletData.playerPositionWhenShot.y;
+
+        float distanceBetweenPoints = std::sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        float radius = distanceBetweenPoints / 2.f;
+
+        float bulletX = ((x1 + x2) / 2) + std::cos((shurikenBulletData.currentTime * multiplier) * (shurikenBulletData.speed/distanceBetweenPoints) + std::atan2(y2 - ((y1 + y2) / 2), x2 - (x1 + x2) / 2)) * radius;
+        float bulletY = ((y1 + y2) / 2) + std::sin((shurikenBulletData.currentTime * multiplier) * (shurikenBulletData.speed/distanceBetweenPoints) + std::atan2(y2 - ((y1 + y2) / 2), x2 - (x1 + x2) / 2)) * radius;
+
+        position.position = {bulletX, bulletY};
+
+        shurikenBulletData.currentTime += deltaTime.asSeconds();
+
+        if ((shurikenBulletData.currentTime * shurikenBulletData.speed) / distanceBetweenPoints > 6.25)
+            registry.destroy(shurikenBulletEntity);
+    }
+}
--- src/systems/BulletSystem.h
+++ src/systems/BulletSystem.h
@@ -11,37 +11,7 @@
 class BulletSystem
 {
 public:
-    static void updateShurikenBullet(entt::registry& registry, sf::Time deltaTime)
-    {
-        auto shurikenBulletsView = registry.view<ShurikenBullet, Position>();
-
-        for (auto shurikenBulletEntity : shurikenBulletsView)
-        {
-            auto& shurikenBulletData = shurikenBulletsView.get<ShurikenBullet>(shurikenBulletEntity);
-            auto& position = shurikenBulletsView.get<Position>(shurikenBulletEntity);
-            
-            int multiplier = shurikenBulletData.right ? 1 : -1;
-
-            float x1 = shurikenBulletData.enemyPositionWhenShot.x;
-            float y1 = shurikenBulletData.enemyPositionWhenShot.y;
-
-            float x2 = shurikenBulletData.playerPositionWhenShot.x;
-            float y2 = shurikenBulletData.playerPositionWhenShot.y;
-
-            float distanceBetweenPoints = std::sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-            float radius = distanceBetweenPoints / 2.f;
-
-            float bulletX = ((x1 + x2) / 2) + std::cos((shurikenBulletData.currentTime * multiplier) * (shurikenBulletData.speed/distanceBetweenPoints) + std::atan2(y2 - ((y1 + y2) / 2), x2 - (x1 + x2) / 2)) * radius;
-            float bulletY = ((y1 + y2) / 2) + std::sin((shurikenBulletData.currentTime * multiplier) * (shurikenBulletData.speed/distanceBetweenPoints) + std::atan2(y2 - ((y1 + y2) / 2), x2 - (x1 + x2) / 2)) * radius;
-
-            position.position = {bulletX, bulletY};
-
-            shurikenBulletData.currentTime += deltaTime.asSeconds();
-
-            if ((shurikenBulletData.currentTime * shurikenBulletData.speed) / distanceBetweenPoints > 6.25)
-                registry.destroy(shurikenBulletEntity);
-        }
-    }
+    static void updateShurikenBullet(entt::registry& registry, sf::Time deltaTime);
 
     template <typename BulletOwnerTag>
     static void createBullet(entt::registry& registry, entt::entity& entity, sf::Vector2f targetPosition, float offset = 0.f)
