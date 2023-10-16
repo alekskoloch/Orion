d17d295d58ef1ec0d85f382e19d42e3a71d31a82
Aleks <aleks.koloch@gmail.com>
2023-10-16 21:55:23 +0200

    Add getMoney function to MoneySystem

--- src/systems/MoneySystem.cpp
+++ src/systems/MoneySystem.cpp
@@ -8,3 +8,8 @@ void MoneySystem::addMoney(entt::registry& registry, int amount)
     auto& money = registry.get<MoneyInventory>(registry.view<Player>()[0]);
     money.money += amount;
 }
+
+unsigned int MoneySystem::getMoney(entt::registry& registry)
+{
+    return registry.get<MoneyInventory>(registry.view<Player>()[0]).money;
+}
--- src/systems/MoneySystem.h
+++ src/systems/MoneySystem.h
@@ -6,4 +6,5 @@ class MoneySystem
 {
 public:
     static void addMoney(entt::registry& registry, int amount);
+    static unsigned int getMoney(entt::registry& registry);
 };
