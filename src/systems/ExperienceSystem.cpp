34d7e56fa8eb1f7f65dceefedfc99f27d3953c6f
Aleks <aleks.koloch@gmail.com>
2023-10-18 09:21:19 +0200

    Update addExp function to include levelup

--- src/systems/ExperienceSystem.cpp
+++ src/systems/ExperienceSystem.cpp
@@ -5,6 +5,15 @@
 
 void ExperienceSystem::addExp(entt::registry& registry, unsigned int exp)
 {
+    auto& playerLevel = registry.get<Experience>(registry.view<Player>()[0]).level;
     auto& playerExp = registry.get<Experience>(registry.view<Player>()[0]).experience;
+    auto& expToNextLevel = registry.get<Experience>(registry.view<Player>()[0]).experienceToNextLevel;
     playerExp += exp;
+
+    while (playerExp >= expToNextLevel)
+    {
+        playerLevel++;
+        playerExp -= registry.get<Experience>(registry.view<Player>()[0]).experienceToNextLevel;
+        expToNextLevel = (unsigned int)(expToNextLevel * 1.5f);
+    }
 }
