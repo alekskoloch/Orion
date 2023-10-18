5c019cbae5c1f923a9e5870fcf78e5c3db494088
Aleks <aleks.koloch@gmail.com>
2023-10-18 09:23:24 +0200

    Update Experience component with skillPoints variable

--- src/components/components.h
+++ src/components/components.h
@@ -71,6 +71,7 @@ struct Experience
     unsigned int level = 1;
     unsigned int experience = 0;
     unsigned int experienceToNextLevel = 100;
+    unsigned int skillPoints = 0;
 };
 
 struct StoneInventory
