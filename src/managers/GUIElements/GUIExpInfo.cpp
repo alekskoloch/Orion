d114392f3bc4cdad66068dd60beb45bf6eb94f52
Aleks <aleks.koloch@gmail.com>
2023-10-18 09:05:04 +0200

    Add GUIExpInfo implementation

--- src/managers/GUIElements/GUIExpInfo.cpp
+++ src/managers/GUIElements/GUIExpInfo.cpp
@@ -1,1 +1,34 @@
 #include "GUIExpInfo.h"
+
+#include "../../components/components.h"
+#include "../../components/tagComponents.h"
+
+GUIExpInfo::GUIExpInfo(sf::RenderWindow& window, entt::registry& registry)
+    : window(window), registry(registry)
+{
+    backgroundBar.setFillColor(sf::Color::Black);
+    backgroundBar.setSize(sf::Vector2f(200, 20));
+    backgroundBar.setOutlineThickness(4);
+    backgroundBar.setOutlineColor(sf::Color::White);
+    backgroundBar.setPosition(200, 50);
+
+    expBar.setFillColor(sf::Color::Yellow);
+    expBar.setSize(sf::Vector2f(0, 20));
+    expBar.setPosition(200, 50);
+}
+
+void GUIExpInfo::update()
+{
+    auto playerEntity = this->registry.view<Player>()[0];
+    auto& playerExp = this->registry.get<Experience>(playerEntity).experience;
+    auto& playerRequiredExp = this->registry.get<Experience>(playerEntity).experienceToNextLevel;
+
+    float expBarWidth = 200.f * (playerExp / (float)playerRequiredExp);
+    expBar.setSize(sf::Vector2f(expBarWidth, 20));
+}
+
+void GUIExpInfo::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+    target.draw(this->backgroundBar, states);
+    target.draw(this->expBar, states);
+}
--- src/managers/GUIElements/GUIExpInfo.h
+++ src/managers/GUIElements/GUIExpInfo.h
@@ -2,7 +2,17 @@
 
 #include "../../pch.h"
 
-class ExpInfo
+class GUIExpInfo : public sf::Drawable
 {
+public:
+    GUIExpInfo(sf::RenderWindow& window, entt::registry& registry);
 
+    void update();
+    void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+private:
+    sf::RenderWindow& window;
+    entt::registry& registry;
+
+    sf::RectangleShape backgroundBar;
+    sf::RectangleShape expBar;
 };
