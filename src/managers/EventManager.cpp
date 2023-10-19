0b0e18dc95c4ee0d39fe9012b188432879acecb8
Aleks <aleks.koloch@gmail.com>
2023-10-19 19:51:36 +0200

    Add EventManager implementation

--- src/managers/EventManager.cpp
+++ src/managers/EventManager.cpp
@@ -1,1 +1,23 @@
 #include "EventManager.h"
+
+EventManager& EventManager::getInstance()
+{
+    static EventManager instance;
+    return instance;
+}
+
+void EventManager::subscribe(Event eventName, EventCallback callback)
+{
+    this->eventSubscribers[eventName].push_back(callback);
+}
+
+void EventManager::trigger(Event eventName)
+{
+    if (this->eventSubscribers.find(eventName) != this->eventSubscribers.end())
+    {
+        for (const auto& callback : this->eventSubscribers[eventName])
+        {
+            callback();
+        }
+    }
+}
--- src/managers/EventManager.h
+++ src/managers/EventManager.h
@@ -4,5 +4,25 @@
 
 class EventManager
 {
+private:
+    EventManager() = default;
 
+public:
+    static EventManager& getInstance();
+
+    enum class Event
+    {
+        NotEnoughEnergy
+    };
+
+    EventManager(EventManager const&) = delete;
+    void operator=(EventManager const&) = delete;
+
+    using EventCallback = std::function<void()>;
+
+    void subscribe(Event eventName, EventCallback callback);
+    void trigger(Event eventName);
+
+private:
+    std::unordered_map<Event, std::vector<EventCallback>> eventSubscribers;
 };
