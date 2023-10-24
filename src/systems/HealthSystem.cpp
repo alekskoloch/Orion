e668aea80f444e00b725ce4ff41c2a8ca975e762
Aleks <aleks.koloch@gmail.com>
2023-10-24 21:15:56 +0200

    Update HealthSystem to play death sound when enemy is destroyed

--- src/systems/HealthSystem.cpp
+++ src/systems/HealthSystem.cpp
@@ -1,5 +1,7 @@
 #include "HealthSystem.h"
 
+#include "../managers/SoundManager.h"
+
 #include "../components/components.h"
 
 void HealthSystem::updateHealth(entt::registry& registry)
@@ -10,6 +12,7 @@ void HealthSystem::updateHealth(entt::registry& registry)
         auto& health = view.get<Health>(entity);
         if (health.currentHealthValue <= 0)
         {
+            SoundManager::getInstance().playSound("Death");
             registry.destroy(entity);
         }
     }
