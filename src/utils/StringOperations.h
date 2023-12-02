310dc70dafd4a78962c6b4f2e76be09181e2e435
Aleks <aleks.koloch@gmail.com>
2023-12-02 21:44:36 +0100

    Add removeWhitespace function to StringOperations

--- src/utils/StringOperations.h
+++ src/utils/StringOperations.h
@@ -1,3 +1,10 @@
 #pragma once
 
 #include "../pch.h"
+
+std::string removeWhitespace(const std::string& str)
+{
+    std::string result = str;
+    result.erase(std::remove_if(result.begin(), result.end(), [](unsigned char c) { return std::isspace(c); }), result.end());
+    return result;
+}
