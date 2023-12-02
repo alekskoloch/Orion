c648efa64353ba58edde906c984eac45cf899d99
Aleks <aleks.koloch@gmail.com>
2023-12-02 18:18:50 +0100

    Refactor SkillSystem by removing individual functions and add SkillType enum

--- src/systems/SkillSystem.cpp
+++ src/systems/SkillSystem.cpp
@@ -3,180 +3,118 @@
 #include "../components/components.h"
 #include "../components/tagComponents.h"
 
-void SkillSystem::addDamageMultiplier(entt::registry& registry, float multiplier)
+void SkillSystem::modifySkill(entt::registry& registry, SkillType skillType, float value)
 {
     auto& playerSkillsComponent = registry.get<Skills>(registry.view<Player>().front());
-    playerSkillsComponent.damageMultiplier += multiplier;
+
+    switch (skillType)
+    {
+    case SkillType::DamageMultiplierValue:
+        playerSkillsComponent.damageMultiplier += value;
+        break;
+    case SkillType::WeaponDamageMultiplierValue:
+        playerSkillsComponent.weaponDamageMultiplier += value;
+        break;
+    case SkillType::SingleShotWeaponDamageMultiplierValue:
+        playerSkillsComponent.singleShotWeaponDamageMultiplier += value;
+        break;
+    case SkillType::TripleShotWeaponDamageMultiplierValue:
+        playerSkillsComponent.tripleShotWeaponDamageMultiplier += value;
+        break;
+    case SkillType::WeaponEnergyCostMultiplierValue:                    
+        playerSkillsComponent.weaponEnergyCostMultiplier += value;
+        break;
+    case SkillType::SingleShotWeaponEnergyCostMultiplierValue:
+        playerSkillsComponent.singleShotWeaponEnergyCostMultiplier += value;
+        break;
+    case SkillType::TripleShotWeaponEnergyCostMultiplierValue:
+        playerSkillsComponent.tripleShotWeaponEnergyCostMultiplier += value;
+        break;
+    case SkillType::SingleSpecialShotWeaponEnergyCostMultiplierValue:
+        playerSkillsComponent.singleSpecialShotWeaponEnergyCostMultiplier += value;
+        break;
+    case SkillType::TripleSpecialShotWeaponEnergyCostMultiplierValue:
+        playerSkillsComponent.tripleSpecialShotWeaponEnergyCostMultiplier += value;
+        break;
+    case SkillType::EnergyRegenerationMultiplierValue:
+        playerSkillsComponent.energyRegenerationMultiplier += value;
+        break;
+    case SkillType::ShieldTimeDurationMultiplierValue:
+        playerSkillsComponent.shieldTimeDurationMultiplier += value;
+        break;
+    case SkillType::SingleShotWeaponSpecialShot:
+        playerSkillsComponent.singleShotWeaponSpecialShot = value == 1;
+        break;
+    case SkillType::TripleShotWeaponSpecialShot:
+        playerSkillsComponent.tripleShotWeaponSpecialShot = value == 1;
+        break;
+    case SkillType::AllWeaponsSpecialShot:
+        playerSkillsComponent.allWeaponsSpecialShot = value == 1;
+        break;
+    case SkillType::ShieldChanceForSingleSpecialShot:
+        playerSkillsComponent.singleSpecialShotWeaponShieldChance = value == 1;
+        break;
+    case SkillType::ShieldChanceForTripleSpecialShot:
+        playerSkillsComponent.tripleSpecialShotWeaponShieldChance = value == 1;
+        break;
+    default:
+        throw std::runtime_error("Skill type not implemented");
+        break;
+    }
 }
 
-void SkillSystem::addWeaponDamageMultiplier(entt::registry& registry, float multiplier)
+bool SkillSystem::isSkillEnabled(entt::registry& registry, SkillType skillType)
 {
     auto& playerSkillsComponent = registry.get<Skills>(registry.view<Player>().front());
-    playerSkillsComponent.weaponDamageMultiplier += multiplier;
+
+    switch (skillType)
+    {
+    case SkillType::SingleShotWeaponSpecialShot:
+        return playerSkillsComponent.singleShotWeaponSpecialShot;
+    case SkillType::TripleShotWeaponSpecialShot:
+        return playerSkillsComponent.tripleShotWeaponSpecialShot;
+    case SkillType::AllWeaponsSpecialShot:
+        return playerSkillsComponent.allWeaponsSpecialShot;
+    case SkillType::ShieldChanceForSingleSpecialShot:
+        return playerSkillsComponent.singleSpecialShotWeaponShieldChance;
+    case SkillType::ShieldChanceForTripleSpecialShot:
+        return playerSkillsComponent.tripleSpecialShotWeaponShieldChance;
+    default:
+        throw std::runtime_error("Skill type not implemented");
+        break;
+    }
 }
 
-void SkillSystem::addSingleShotWeaponDamageMultiplier(entt::registry& registry, float multiplier)
+float SkillSystem::getValue(entt::registry& registry, SkillType skillType)
 {
     auto& playerSkillsComponent = registry.get<Skills>(registry.view<Player>().front());
-    playerSkillsComponent.singleShotWeaponDamageMultiplier += multiplier;
-}
 
-float SkillSystem::getWeaponDamage(entt::registry& registry, entt::entity entity)
-{
-    auto& skillsComponent = registry.get<Skills>(entity);
-    auto& weaponComponent = registry.get<Weapon>(entity);
-
-    float weaponDamage = weaponComponent.damage;
-    float damageMultiplier = skillsComponent.weaponDamageMultiplier;
-    
-    if (weaponComponent.weaponType == WeaponType::SingleShot)
-        damageMultiplier += skillsComponent.singleShotWeaponDamageMultiplier - 1;
-
-    damageMultiplier += skillsComponent.damageMultiplier - 1;
-
-    return weaponDamage * damageMultiplier;
-}
-
-void SkillSystem::addWeaponEnergyCostMultiplier(entt::registry& registry, float multiplier)
-{
-    auto& playerSkillsComponent = registry.get<Skills>(registry.view<Player>().front());
-    playerSkillsComponent.weaponEnergyCostMultiplier += multiplier;
-}
-
-void SkillSystem::addSingleShotWeaponEnergyCostMultiplier(entt::registry& registry, float multiplier)
-{
-    auto& playerSkillsComponent = registry.get<Skills>(registry.view<Player>().front());
-    playerSkillsComponent.singleShotWeaponEnergyCostMultiplier += multiplier;
-}
-
-void SkillSystem::addSingleShotWeaponSpecialShotEnergyCostMultiplier(entt::registry& registry, float multiplier)
-{
-    auto& playerSkillsComponent = registry.get<Skills>(registry.view<Player>().front());
-    playerSkillsComponent.singleSpecialShotWeaponEnergyCostMultiplier += multiplier;
-}
-
-void SkillSystem::addTripleShotWeaponSpecialShotEnergyCostMultiplier(entt::registry& registry, float multiplier)
-{
-    auto& playerSkillsComponent = registry.get<Skills>(registry.view<Player>().front());
-    playerSkillsComponent.tripleSpecialShotWeaponEnergyCostMultiplier += multiplier;
-}
-
-float SkillSystem::getWeaponEnergyCost(entt::registry& registry, entt::entity entity)
-{
-    auto& skillsComponent = registry.get<Skills>(entity);
-    auto& weaponComponent = registry.get<Weapon>(entity);
-
-    float energyCost = weaponComponent.energyCost;
-    float energyCostMultiplier = 1.f;
-
-    if (weaponComponent.weaponType == WeaponType::SingleShot)
-        energyCostMultiplier += skillsComponent.singleShotWeaponEnergyCostMultiplier - 1;
-
-    return energyCost * energyCostMultiplier;
-}
-
-float SkillSystem::getWeaponSpecialShotEnergyCost(entt::registry& registry, entt::entity entity)
-{
-    auto& skillsComponent = registry.get<Skills>(entity);
-    auto& weaponComponent = registry.get<Weapon>(entity);
-
-    float energyCost = weaponComponent.energyCostForSpecialShot;
-    float energyCostMultiplier = 1.f;
-
-    if (weaponComponent.weaponType == WeaponType::SingleShot)
-        energyCostMultiplier += skillsComponent.singleSpecialShotWeaponEnergyCostMultiplier - 1;
-    else if (weaponComponent.weaponType == WeaponType::TrippleShot)
-        energyCostMultiplier += skillsComponent.tripleSpecialShotWeaponEnergyCostMultiplier - 1;
-
-    return energyCost * energyCostMultiplier;
-}
-
-void SkillSystem::addEnergyRegenerationMultiplier(entt::registry& registry, float multiplier)
-{
-    auto& playerSkillsComponent = registry.get<Skills>(registry.view<Player>().front());
-    playerSkillsComponent.energyRegenerationMultiplier += multiplier;
-}
-
-float SkillSystem::getEnergyRegeneration(entt::registry& registry, entt::entity entity)
-{
-    auto& skillsComponent = registry.get<Skills>(entity);
-    auto& energyComponent = registry.get<Energy>(entity);
-
-    float energyRegeneration = energyComponent.energyRegenerationRate;
-    float energyRegenerationMultiplier = skillsComponent.energyRegenerationMultiplier;
-
-    return energyRegeneration * energyRegenerationMultiplier;
-}
-
-void SkillSystem::addShieldTimeDurationMultiplier(entt::registry& registry, float multiplier)
-{
-    auto& playerSkillsComponent = registry.get<Skills>(registry.view<Player>().front());
-    playerSkillsComponent.shieldTimeDurationMultiplier += multiplier;
-}
-
-float SkillSystem::getShieldTimeDurationMultiplier(entt::registry& registry)
-{
-    auto playerSkillsComponent = registry.get<Skills>(registry.view<Player>().front());
-    return playerSkillsComponent.shieldTimeDurationMultiplier;
-}
-
-void SkillSystem::enableSingleShotWeaponSpecialShot(entt::registry& registry)
-{
-    auto& playerSkillsComponent = registry.get<Skills>(registry.view<Player>().front());
-    playerSkillsComponent.singleShotWeaponSpecialShot = true;
-}
-
-void SkillSystem::enableTripleShotWeaponSpecialShot(entt::registry& registry)
-{
-    auto& playerSkillsComponent = registry.get<Skills>(registry.view<Player>().front());
-    playerSkillsComponent.tripleShotWeaponSpecialShot = true;
-}
-
-void SkillSystem::enableAllWeaponsSpecialShot(entt::registry& registry)
-{
-    auto& playerSkillsComponent = registry.get<Skills>(registry.view<Player>().front());
-    playerSkillsComponent.allWeaponsSpecialShot = true;
-}
-
-bool SkillSystem::isSingleShotWeaponSpecialShotEnabled(entt::registry& registry)
-{
-    auto playerSkillsComponent = registry.get<Skills>(registry.view<Player>().front());
-    return playerSkillsComponent.singleShotWeaponSpecialShot;
-}
-
-bool SkillSystem::isTripleShotWeaponSpecialShotEnabled(entt::registry& registry)
-{
-    auto playerSkillsComponent = registry.get<Skills>(registry.view<Player>().front());
-    return playerSkillsComponent.tripleShotWeaponSpecialShot;
-}
-
-bool SkillSystem::isAllWeaponsSpecialShotEnabled(entt::registry& registry)
-{
-    auto playerSkillsComponent = registry.get<Skills>(registry.view<Player>().front());
-    return playerSkillsComponent.allWeaponsSpecialShot;
-}
-
-void SkillSystem::enableShieldChanceForSingleSpecialShot(entt::registry& registry)
-{
-    auto& playerSkillsComponent = registry.get<Skills>(registry.view<Player>().front());
-    playerSkillsComponent.singleSpecialShotWeaponShieldChance = true;
-}
-
-void SkillSystem::enableShieldChanceForTripleSpecialShot(entt::registry& registry)
-{
-    auto& playerSkillsComponent = registry.get<Skills>(registry.view<Player>().front());
-    playerSkillsComponent.tripleSpecialShotWeaponShieldChance = true;
-}
-
-bool SkillSystem::getShieldChance(entt::registry& registry, entt::entity entity)
-{
-    auto& skillsComponent = registry.get<Skills>(entity);
-    auto& weaponComponent = registry.get<Weapon>(entity);
-
-    if (weaponComponent.weaponType == WeaponType::SingleShot)
-        return skillsComponent.singleSpecialShotWeaponShieldChance;
-    else if (weaponComponent.weaponType == WeaponType::TrippleShot)
-        return skillsComponent.tripleSpecialShotWeaponShieldChance;
-    else
-        return false;
+    switch (skillType)
+    {
+    case SkillType::DamageMultiplierValue:
+        return playerSkillsComponent.damageMultiplier;
+    case SkillType::WeaponDamageMultiplierValue:
+        return playerSkillsComponent.weaponDamageMultiplier;
+    case SkillType::SingleShotWeaponDamageMultiplierValue:
+        return playerSkillsComponent.singleShotWeaponDamageMultiplier;
+    case SkillType::TripleShotWeaponDamageMultiplierValue:
+        return playerSkillsComponent.tripleShotWeaponDamageMultiplier;
+    case SkillType::WeaponEnergyCostMultiplierValue:
+        return playerSkillsComponent.weaponEnergyCostMultiplier;
+    case SkillType::SingleShotWeaponEnergyCostMultiplierValue:
+        return playerSkillsComponent.singleShotWeaponEnergyCostMultiplier;
+    case SkillType::TripleShotWeaponEnergyCostMultiplierValue:
+        return playerSkillsComponent.tripleShotWeaponEnergyCostMultiplier;
+    case SkillType::SingleSpecialShotWeaponEnergyCostMultiplierValue:
+        return playerSkillsComponent.singleSpecialShotWeaponEnergyCostMultiplier;
+    case SkillType::TripleSpecialShotWeaponEnergyCostMultiplierValue:
+        return playerSkillsComponent.tripleSpecialShotWeaponEnergyCostMultiplier;
+    case SkillType::EnergyRegenerationMultiplierValue:
+        return playerSkillsComponent.energyRegenerationMultiplier;
+    case SkillType::ShieldTimeDurationMultiplierValue:
+        return playerSkillsComponent.shieldTimeDurationMultiplier;
+    default:
+        throw std::runtime_error("Skill type not implemented");
+        break;
+    }
 }
