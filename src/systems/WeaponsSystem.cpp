05ebc150ac8312b07bfa1097f18ae23df42abf3e
Aleks <aleks.koloch@gmail.com>
2023-12-02 18:21:09 +0100

    Update WeaponsSystem with weapon related value returning functions

--- src/systems/WeaponsSystem.cpp
+++ src/systems/WeaponsSystem.cpp
@@ -3,6 +3,7 @@
 #include "../managers/TextureManager.h"
 
 #include "../systems/PlayerInitializationSystem.h"
+#include "../systems/SkillSystem.h"
 
 #include "../components/components.h"
 #include "../components/tagComponents.h"
@@ -38,4 +39,74 @@ void WeaponsSystem::updateWeaponCooldown(entt::registry& registry, sf::Time delt
                 weapon.currentCooldownTime = 0.f;
         }
     }
+}
+
+float WeaponsSystem::getWeaponDamage(entt::registry& registry)
+{
+    auto& playerSkillsComponent = registry.get<Skills>(registry.view<Player>().front());
+    
+    auto& playerWeapon = registry.get<Weapon>(registry.view<Player>().front());
+
+    float damage = playerWeapon.damage;
+    float multiplier = 1.f;
+
+    multiplier += playerSkillsComponent.damageMultiplier - 1.f;
+    multiplier += playerSkillsComponent.weaponDamageMultiplier - 1.f;
+
+    if (playerWeapon.weaponType == WeaponType::SingleShot)
+    {
+        multiplier += playerSkillsComponent.singleShotWeaponDamageMultiplier - 1.f;
+    }
+    else if (playerWeapon.weaponType == WeaponType::TrippleShot)
+    {
+        multiplier += playerSkillsComponent.tripleShotWeaponDamageMultiplier - 1.f;
+    }
+
+    return damage * multiplier;
+}
+
+float WeaponsSystem::getWeaponShotEnergyCost(entt::registry& registry)
+{
+    auto& playerSkillsComponent = registry.get<Skills>(registry.view<Player>().front());
+
+    auto& playerWeapon = registry.get<Weapon>(registry.view<Player>().front());
+
+    float energyCost = playerWeapon.energyCost;
+    float multiplier = 1.f;
+
+    multiplier += playerSkillsComponent.weaponEnergyCostMultiplier - 1.f;
+
+    if (playerWeapon.weaponType == WeaponType::SingleShot)
+    {
+        multiplier += playerSkillsComponent.singleShotWeaponEnergyCostMultiplier - 1.f;
+    }
+    else if (playerWeapon.weaponType == WeaponType::TrippleShot)
+    {
+        multiplier += playerSkillsComponent.tripleShotWeaponEnergyCostMultiplier - 1.f;
+    }
+
+    return energyCost * multiplier;
+}
+
+float WeaponsSystem::getWeaponSpecialShotEnergyCost(entt::registry& registry)
+{
+    auto& playerSkillsComponent = registry.get<Skills>(registry.view<Player>().front());
+
+    auto& playerWeapon = registry.get<Weapon>(registry.view<Player>().front());
+
+    float energyCost = playerWeapon.energyCostForSpecialShot;
+    float multiplier = 1.f;
+
+    multiplier += playerSkillsComponent.weaponEnergyCostMultiplier - 1.f;
+
+    if (playerWeapon.weaponType == WeaponType::SingleShot)
+    {
+        multiplier += playerSkillsComponent.singleSpecialShotWeaponEnergyCostMultiplier - 1.f;
+    }
+    else if (playerWeapon.weaponType == WeaponType::TrippleShot)
+    {
+        multiplier += playerSkillsComponent.tripleSpecialShotWeaponEnergyCostMultiplier - 1.f;
+    }
+
+    return energyCost * multiplier;
 }
--- src/systems/WeaponsSystem.h
+++ src/systems/WeaponsSystem.h
@@ -10,4 +10,7 @@ public:
     static void changeWeapon(entt::registry& registry, WeaponSchema weapon);
     static void loadWeapon(entt::registry& registry, const WeaponSchema& weaponSchema, entt::entity ownerEntity);
     static void updateWeaponCooldown(entt::registry& registry, sf::Time deltaTime);
+    static float getWeaponDamage(entt::registry& registry);
+    static float getWeaponShotEnergyCost(entt::registry& registry);
+    static float getWeaponSpecialShotEnergyCost(entt::registry& registry);
 };
