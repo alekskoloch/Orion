7b30980d703785bbef8db971681b545a9a875665
Aleks <aleks.koloch@gmail.com>
2023-12-14 19:57:29 +0100

    Update EnemyInitializationSystem to load new enemies from enemyConfig.json

--- src/systems/EnemyInitializationSystem.cpp
+++ src/systems/EnemyInitializationSystem.cpp
@@ -135,93 +135,72 @@ void EnemyInitializationSystem::createEnemy(entt::registry& registry, const Enem
 //TODO: Temporary solution for initialize enemy, refactor later
 void EnemyInitializationSystem::createNewEnemy(entt::registry& registry, sf::Vector2f position)
 {
-    EnemySchema enemySchema = enemy;
-    switch (ProceduralGenerationSystem::GetRandomNumber(1, 13))
+    std::string enemyName = "enemy1";
+
+    std::ifstream configFile(CONFIG_PATH + std::string("enemyConfig.json"));
+    if (!configFile.is_open())
+        throw std::runtime_error("Could not open config file");
+    else
+        EnemyInitializationSystem::loadEnemyFromConfig(registry, enemyName, configFile, position);
+}
+
+void EnemyInitializationSystem::loadEnemyFromConfig(entt::registry& registry, std::string enemyName, std::ifstream& configFile, sf::Vector2f position)
+{
+    nlohmann::json configJson;
+    configFile >> configJson;
+
+    if (!configJson.contains("enemies") || !configJson["enemies"].is_array())
+        throw std::runtime_error("Could not find enemies array in config file");
+    else
     {
-        case 1:
-            enemySchema = enemy;
-            break;
-        case 2:
-            enemySchema = enemy2;
-            break;
-        case 3:
-            enemySchema = enemy3;
-            break;
-        case 4:
-            enemySchema = enemy4;
-            break;
-        case 5:
-            enemySchema = enemyRed1;
-            break;
-        case 6:
-            enemySchema = enemyRed2;
-            break;
-        case 7:
-            enemySchema = enemyRed3;
-            break;
-        case 8:
-            enemySchema = enemyRed4;
-            break;
-        case 9:
-            enemySchema = enemyZombie1;
-            break;
-        case 10:
-            enemySchema = enemyZombie2;
-            break;
-        case 11:
-            enemySchema = enemyZombie3;
-            break;
-        case 12:
-            enemySchema = enemyZombie4;
-            break;
-        case 13:
-            enemySchema = enemyZombie5;
-            break;
-        default:
-            break;
+        for (const auto& enemyJson : configJson["enemies"])
+        {
+            if (enemyJson.contains("name") && enemyJson["name"] == enemyName)
+            {
+                auto enemy = registry.create();
+
+                registry.emplace<Enemy>(enemy);
+                registry.emplace<EntityState>(enemy);
+
+                auto& enemyState = registry.get<EntityState>(enemy);
+                enemyState.attackRange = enemyJson["attackRange"].get<float>();
+                enemyState.idleRange = enemyJson["idleRange"].get<float>();
+
+                sf::Sprite sprite = CreateSprite(enemyJson["name"].get<std::string>());
+                registry.emplace<Collision>(enemy, sprite.getGlobalBounds());
+                registry.emplace<Renderable>(enemy, sprite);
+
+                registry.emplace<Position>(enemy, position);
+                registry.emplace<Speed>(enemy, enemyJson["speed"].get<float>());
+                registry.emplace<Velocity>(enemy, sf::Vector2f(0.f, 0.f));
+                registry.emplace<Name>(enemy, enemyJson["name"].get<std::string>());
+                registry.emplace<Health>(enemy, enemyJson["health"].get<float>(), enemyJson["health"].get<float>());
+
+                unsigned int LEVEL = 1;
+                registry.emplace<Experience>(enemy, LEVEL, enemyJson["exp"].get<unsigned int>());
+                registry.emplace<Drop>(enemy, 100, 69);
+
+                //if enemyJons weapon == basicEnemyWeapon
+                if (enemyJson["weapon"] == "basicEnemyWeapon")
+                {
+                    WeaponsSystem::loadWeapon(registry, basicEnemyWeapon, enemy);
+                }
+                
+                int numOfWaypoints = ProceduralGenerationSystem::GetRandomNumber(2, 6);
+                std::vector<sf::Vector2f> waypoints;
+
+                auto enemyPos = registry.get<Position>(enemy);
+
+                for (int i = 0; i < numOfWaypoints; i++)
+                {
+                    float x = ProceduralGenerationSystem::GetRandomNumber(enemyPos.position.x - 500.f, enemyPos.position.x + 500.f);
+                    float y = ProceduralGenerationSystem::GetRandomNumber(enemyPos.position.y - 500.f, enemyPos.position.y + 500.f);
+
+                    waypoints.push_back(sf::Vector2f(x, y));
+                }
+
+                registry.emplace<WaypointMovement>(enemy, waypoints);
+            }
+        }
     }
-
-    TextureManager::getInstance().loadTexture(enemySchema.textureName, ASSETS_PATH + enemySchema.textureName + ".png");
-    TextureManager::getInstance().loadTexture("enemy_bullet", ASSETS_PATH + std::string("enemy_bullet") + ".png");
-
-    auto enemy = registry.create();
-    registry.emplace<Enemy>(enemy);
-    registry.emplace<EntityState>(enemy);
-
-    //TODO: Temporary solution to set ranges
-    auto& enemyState = registry.get<EntityState>(enemy);
-    enemyState.attackRange = enemySchema.attackRange;
-    enemyState.idleRange = enemySchema.idleRange;
-
-    sf::Sprite sprite = CreateSprite(enemySchema.textureName);
-    registry.emplace<Collision>(enemy, sprite.getGlobalBounds());
-    registry.emplace<Renderable>(enemy, sprite);
-
-    registry.emplace<Position>(enemy, position);
-    registry.emplace<Speed>(enemy, enemySchema.speed);
-    registry.emplace<Velocity>(enemy, enemySchema.velocity);
-    registry.emplace<Name>(enemy, enemySchema.name);
-    registry.emplace<Health>(enemy, enemySchema.health, enemySchema.health);
-
-    unsigned int LEVEL = 1;
-    registry.emplace<Experience>(enemy, LEVEL, enemySchema.experience);
-    registry.emplace<Drop>(enemy, 100, 69);
-
-    WeaponsSystem::loadWeapon(registry, enemySchema.weaponSchema, enemy);
-
-    //TODO: Temporary solution for waypoints
-    int numOfWaypoints = ProceduralGenerationSystem::GetRandomNumber(2, 6);
-    std::vector<sf::Vector2f> waypoints;
-
-    auto enemyPos = registry.get<Position>(enemy);
-
-    for (int i = 0; i < numOfWaypoints; i++)
-    {
-        float x = ProceduralGenerationSystem::GetRandomNumber(enemyPos.position.x - 500.f, enemyPos.position.x + 500.f);
-        float y = ProceduralGenerationSystem::GetRandomNumber(enemyPos.position.y - 500.f, enemyPos.position.y + 500.f);
-
-        waypoints.push_back(sf::Vector2f(x, y));
-    }
-
-    registry.emplace<WaypointMovement>(enemy, waypoints);
 }
