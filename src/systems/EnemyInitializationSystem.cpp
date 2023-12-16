db07fefe11b965b08a0970088373177eae912690
Aleks <aleks.koloch@gmail.com>
2023-12-16 23:36:35 +0100

    Update EnemyInitializationSystem to use EnemyBuilder

--- src/systems/EnemyInitializationSystem.cpp
+++ src/systems/EnemyInitializationSystem.cpp
@@ -2,6 +2,8 @@
 
 #include "ProceduralGenerationSystem.h"
 
+#include "../entities/EnemyBuilder.h"
+
 #include "../managers/TextureManager.h"
 #include "../systems/WeaponsSystem.h"
 #include "../components/components.h"
@@ -48,49 +50,17 @@ void EnemyInitializationSystem::loadEnemyFromConfig(entt::registry& registry, st
         {
             if (enemyJson.contains("name") && enemyJson["name"] == enemyName)
             {
-                auto enemy = registry.create();
+                EnemyBuilder enemyBuilder(registry);
 
-                registry.emplace<Enemy>(enemy);
-                registry.emplace<EntityState>(enemy);
-
-                auto& enemyState = registry.get<EntityState>(enemy);
-                enemyState.attackRange = enemyJson["attackRange"].get<float>();
-                enemyState.idleRange = enemyJson["idleRange"].get<float>();
-
-                sf::Sprite sprite = CreateSprite(enemyJson["name"].get<std::string>());
-                registry.emplace<Collision>(enemy, sprite.getGlobalBounds());
-                registry.emplace<Renderable>(enemy, sprite);
-
-                registry.emplace<Position>(enemy, position);
-                registry.emplace<Speed>(enemy, enemyJson["speed"].get<float>());
-                registry.emplace<Velocity>(enemy, sf::Vector2f(0.f, 0.f));
-                registry.emplace<Name>(enemy, enemyJson["name"].get<std::string>());
-                registry.emplace<Health>(enemy, enemyJson["health"].get<float>(), enemyJson["health"].get<float>());
-
-                unsigned int LEVEL = 1;
-                registry.emplace<Experience>(enemy, LEVEL, enemyJson["exp"].get<unsigned int>());
-                registry.emplace<Drop>(enemy, 100, 69);
-
-                //if enemyJons weapon == basicEnemyWeapon
-                if (enemyJson["weapon"] == "basicEnemyWeapon")
-                {
-                    WeaponsSystem::loadWeapon(registry, basicEnemyWeapon, enemy);
-                }
-                
-                int numOfWaypoints = ProceduralGenerationSystem::GetRandomNumber(2, 6);
-                std::vector<sf::Vector2f> waypoints;
-
-                auto enemyPos = registry.get<Position>(enemy);
-
-                for (int i = 0; i < numOfWaypoints; i++)
-                {
-                    float x = ProceduralGenerationSystem::GetRandomNumber(enemyPos.position.x - 500.f, enemyPos.position.x + 500.f);
-                    float y = ProceduralGenerationSystem::GetRandomNumber(enemyPos.position.y - 500.f, enemyPos.position.y + 500.f);
-
-                    waypoints.push_back(sf::Vector2f(x, y));
-                }
-
-                registry.emplace<WaypointMovement>(enemy, waypoints);
+                enemyBuilder.createEnemy(enemyJson["name"])
+                            .addEntityState(enemyJson["attackRange"], enemyJson["idleRange"])
+                            .addPosition(position)
+                            .addSpeed(enemyJson["speed"])
+                            .addHealth(enemyJson["health"])
+                            .addExperience(enemyJson["experience"])
+                            .addDrop(100, 69)
+                            .setWeapon(basicEnemyWeapon)
+                            .setWaypointMovement();
             }
         }
     }
