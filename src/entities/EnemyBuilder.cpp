564fbff1542a6cb7d749da3238b69229f985c8e4
Aleks <aleks.koloch@gmail.com>
2023-12-16 23:36:01 +0100

    Add EnemyBuilder implementation

--- src/entities/EnemyBuilder.cpp
+++ src/entities/EnemyBuilder.cpp
@@ -1,8 +1,100 @@
 #include "EnemyBuilder.h"
 
-EnemyBuilder::EnemyBuilder(entt::registry& registry, entt::entity enemy)
-    : registry(registry), enemy(enemy)
+#include "../systems/WeaponsSystem.h"
+#include "../systems/ProceduralGenerationSystem.h"
+
+#include "../utils/GraphicsOperations.h"
+
+EnemyBuilder::EnemyBuilder(entt::registry& registry) : registry(registry)
 {
+    
+}
+
+EnemyBuilder& EnemyBuilder::createEnemy(std::string name)
+{
+    enemy = registry.create();
+
     registry.emplace<Enemy>(enemy);
+    registry.emplace<Name>(enemy);
+    
+    sf::Sprite sprite = CreateSprite(name);
+    registry.emplace<Collision>(enemy, sprite.getGlobalBounds());
+    registry.emplace<Renderable>(enemy, sprite);
+
+    return *this;
+}
+
+EnemyBuilder& EnemyBuilder::addEntityState(float attackRange, float idleRange)
+{
     registry.emplace<EntityState>(enemy);
+    auto& enemyState = registry.get<EntityState>(enemy);
+    enemyState.attackRange = attackRange;
+    enemyState.idleRange = idleRange;
+
+    return *this;
+}
+
+EnemyBuilder& EnemyBuilder::addPosition(sf::Vector2f position)
+{
+    registry.emplace<Position>(enemy, position);
+
+    return *this;
+}
+
+EnemyBuilder& EnemyBuilder::addSpeed(float speed)
+{
+    registry.emplace<Speed>(enemy, speed);
+    registry.emplace<Velocity>(enemy, sf::Vector2f(0.f, 0.f));
+
+    return *this;
+}
+
+EnemyBuilder& EnemyBuilder::addHealth(float health)
+{
+    registry.emplace<Health>(enemy, health, health);
+
+    return *this;
+}
+
+EnemyBuilder& EnemyBuilder::addExperience(float experience)
+{
+    //TODO: Level is temporary
+    registry.emplace<Experience>(enemy, 1, experience);
+
+    return *this;
+}
+
+EnemyBuilder& EnemyBuilder::addDrop(int dropChance, int dropRate)
+{
+    registry.emplace<Drop>(enemy, dropChance, dropRate);
+
+    return *this;
+}
+
+EnemyBuilder& EnemyBuilder::setWeapon(WeaponSchema weaponSchema)
+{
+    WeaponsSystem::loadWeapon(this->registry, weaponSchema, this->enemy);
+
+    return *this;
+}
+
+EnemyBuilder& EnemyBuilder::setWaypointMovement()
+{
+    // TODO: Move this to a separate function
+    int numOfWaypoints = ProceduralGenerationSystem::GetRandomNumber(2, 6);
+    std::vector<sf::Vector2f> waypoints;
+
+    auto enemyPos = registry.get<Position>(enemy);
+
+    for (int i = 0; i < numOfWaypoints; i++)
+    {
+        float x = ProceduralGenerationSystem::GetRandomNumber(enemyPos.position.x - 500.f, enemyPos.position.x + 500.f);
+        float y = ProceduralGenerationSystem::GetRandomNumber(enemyPos.position.y - 500.f, enemyPos.position.y + 500.f);
+
+        waypoints.push_back(sf::Vector2f(x, y));
+    }
+
+    registry.emplace<WaypointMovement>(enemy, waypoints);
+
+    return *this;
 }
--- src/entities/EnemyBuilder.h
+++ src/entities/EnemyBuilder.h
@@ -2,13 +2,25 @@
 
 #include "../pch.h"
 
+#include "../schema/WeaponsSchema.h"
+
 #include "../components/components.h"
 #include "../components/tagComponents.h"
 
 class EnemyBuilder
 {
 public:
-    EnemyBuilder(entt::registry& registry, entt::entity enemy);
+    EnemyBuilder(entt::registry& registry);
+
+    EnemyBuilder& createEnemy(std::string name);
+    EnemyBuilder& addEntityState(float attackRange, float idleRange);
+    EnemyBuilder& addPosition(sf::Vector2f position);
+    EnemyBuilder& addSpeed(float speed);
+    EnemyBuilder& addHealth(float health);
+    EnemyBuilder& addExperience(float experience);
+    EnemyBuilder& addDrop(int dropChance, int dropRate);
+    EnemyBuilder& setWeapon(WeaponSchema weaponSchema);
+    EnemyBuilder& setWaypointMovement();
 
 private:
     entt::registry& registry;
