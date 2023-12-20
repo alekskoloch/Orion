3ef950a6969f24fc97e28c1fec04c7daa80b5785
Aleks <aleks.koloch@gmail.com>
2023-12-20 22:02:41 +0100

    Add Weapons enum class

--- src/systems/WeaponsSystem.h
+++ src/systems/WeaponsSystem.h
@@ -4,11 +4,17 @@
 
 #include "../schema/WeaponsSchema.h"
 
+enum class Weapons
+{
+    RedWeapon
+};
+
 class WeaponsSystem
 {
 public:
     static void changeWeapon(entt::registry& registry, WeaponSchema weapon);
     static void loadWeapon(entt::registry& registry, const WeaponSchema& weaponSchema, entt::entity ownerEntity);
+    static void loadWeaponFromConfig(entt::registry& registry, std::ifstream& configFile, entt::entity ownerEntity);
     static void updateWeaponCooldown(entt::registry& registry, sf::Time deltaTime);
     static float getWeaponDamage(entt::registry& registry);
     static float getWeaponShotEnergyCost(entt::registry& registry);
