1a6bd94c9f4a657487a331d75c1d15daff39f232
Aleks <aleks.koloch@gmail.com>
2023-12-20 23:00:12 +0100

    Add ShotType enum class

--- src/systems/WeaponsSystem.h
+++ src/systems/WeaponsSystem.h
@@ -9,12 +9,26 @@ enum class Weapons
     RedWeapon
 };
 
+enum class ShotType
+{
+    /* 0 */     None = 0,
+//              BASIC TYPES OF SHOTS
+    /* 1 */     SingleShot = 1,
+    /* 2 */     TripleShot,
+    /* 3 */     Shuriken,
+    /* 4 */     DoubleShuriken,
+    /* 5 */     Nail,
+//              SPECIAL TYPES OF SHOTS
+    /* 100 */   FullCircle = 100,
+    /* 101 */   TripleSalvo
+};
+
 class WeaponsSystem
 {
 public:
     static void changeWeapon(entt::registry& registry, WeaponSchema weapon);
     static void loadWeapon(entt::registry& registry, const WeaponSchema& weaponSchema, entt::entity ownerEntity);
-    static void loadWeaponFromConfig(entt::registry& registry, std::ifstream& configFile, entt::entity ownerEntity);
+    static void loadWeaponFromConfig(entt::registry& registry, entt::entity ownerEntity, Weapons weaponID);
     static void updateWeaponCooldown(entt::registry& registry, sf::Time deltaTime);
     static float getWeaponDamage(entt::registry& registry);
     static float getWeaponShotEnergyCost(entt::registry& registry);
