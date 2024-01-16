b2c5ddf3f939d2b384d53b4d22e64e1973a2e3ea
Aleks <aleks.koloch@gmail.com>
2024-01-16 22:35:26 +0100

    Add NotifySystem basic implementation

--- src/systems/NotifySystem.cpp
+++ src/systems/NotifySystem.cpp
@@ -1,1 +1,35 @@
 #include "NotifySystem.h"
+
+void NotifySystem::notify(const std::string& message, float displayTime)
+{
+    sf::Text text(message, FontManager::getInstance().getFont("font"), 24); //TODO: set font size
+    text.setPosition(20.f, 1080.f);
+
+    if (!notifications.empty())
+        for (auto& notification : notifications)
+            notification.text.move(0.f, notification.text.getGlobalBounds().height + 20.f); //TODO: set margin
+
+    notifications.push_back({ text });
+}
+
+void NotifySystem::update(sf::Time deltaTime)
+{
+    for (auto& notification : notifications)
+    {
+        notification.displayTime -= deltaTime.asSeconds();
+
+        if (notification.displayTime < 1.f)
+            notification.text.setFillColor(sf::Color(255, 255, 255, 255 * notification.displayTime));
+    }
+
+    notifications.remove_if([](const Notification& notification)
+    {
+        return notification.displayTime <= 0.f;
+    });
+}
+
+void NotifySystem::draw(sf::RenderWindow& window)
+{
+    for (const auto& notification : notifications)
+            window.draw(notification.text);
+}
--- src/systems/NotifySystem.h
+++ src/systems/NotifySystem.h
@@ -2,10 +2,25 @@
 
 #include "../pch.h"
 
+#include "../components/components.h"
+
+#include "../managers/FontManager.h"
+
+#include "CooldownSystem.h"
+
 class NotifySystem
 {
 public:
+    struct Notification
+    {
+        sf::Text text;
+        float displayTime = 4.f;
+    };
 
+    static void notify(const std::string& message, float displayTime = 4.f);
+    static void update(sf::Time deltaTime);
+    static void draw(sf::RenderWindow& window);
+    
 private:
-
+    static inline std::list<Notification> notifications;
 };
