173fcf1901e890c9ae1dd287c1a20cd30b96f7cf
Aleks <aleks.koloch@gmail.com>
2024-01-30 19:26:32 +0100

    Add QuestStage struct

--- src/systems/QuestCore/QuestStage.h
+++ src/systems/QuestCore/QuestStage.h
@@ -1,3 +1,19 @@
 #pragma once
 
 #include "../../pch.h"
+
+#include "QuestCondition.h"
+
+struct QuestStage
+{
+    std::string description;
+    std::function<void(entt::registry& registry)> action;
+    std::shared_ptr<IQuestCondition> condition;
+
+    bool actionExecuted = false;
+    bool completed = false;
+    bool isCompleted(entt::registry& registry)
+    {
+        return condition->check(registry);
+    }
+};
