484125c99810eab3e06da2f9adf55172082b0429
Aleks <aleks.koloch@gmail.com>
2024-01-30 19:28:29 +0100

    Add Quest struct

--- src/systems/QuestCore/Quest.h
+++ src/systems/QuestCore/Quest.h
@@ -1,3 +1,14 @@
 #pragma once
 
 #include "../../pch.h"
+
+#include "QuestStage.h"
+
+struct Quest
+{
+    std::string name;
+    std::vector<QuestStage> stages;
+    unsigned int currentStage = 0;
+    bool completed = false;
+    bool active = false;
+};
