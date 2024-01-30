63714935b2a1015b5d9f601bd44dc30ef78b8536
Aleks <aleks.koloch@gmail.com>
2024-01-30 19:27:24 +0100

    Add QuestStageBuilder

--- src/systems/QuestCore/QuestStage.h
+++ src/systems/QuestCore/QuestStage.h
@@ -17,3 +17,32 @@ struct QuestStage
         return condition->check(registry);
     }
 };
+
+class QuestStageBuilder
+{
+public:
+    QuestStageBuilder() {}
+
+    QuestStageBuilder& addDescription(std::string description)
+    {
+        stage.description = description;
+        return *this;
+    }
+    QuestStageBuilder& addAction(std::function<void(entt::registry& registry)> action)
+    {
+        stage.action = action;
+        return *this;
+    }
+    QuestStageBuilder& addCondition(const std::shared_ptr<IQuestCondition>& condition)
+    {
+        stage.condition = condition;
+        return *this;
+    }
+    
+    QuestStage build()
+    {
+        return stage;
+    }
+private:
+    QuestStage stage;
+};
