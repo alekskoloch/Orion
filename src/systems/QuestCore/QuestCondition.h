c0afa91a9c015c71d55b7f39a4e3c4d115ae4939
Aleks <aleks.koloch@gmail.com>
2024-01-30 19:12:33 +0100

    Add IQuestCondition

--- src/systems/QuestCore/QuestCondition.h
+++ src/systems/QuestCore/QuestCondition.h
@@ -1,1 +1,17 @@
 #pragma once
+
+#include "../../pch.h"
+
+class IQuestCondition
+{
+public:
+    virtual ~IQuestCondition() = default;
+    virtual bool check(entt::registry& registry) = 0;
+
+    virtual void subscribeEvent() = 0;
+    virtual void unsubscribeEvent() = 0;
+
+    virtual std::string getProgress() { return ""; }
+protected:
+    unsigned int subscriberId;
+};
