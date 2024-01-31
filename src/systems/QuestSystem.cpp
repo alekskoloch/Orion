0ad44017513c02aa5a3e4e92e95e7b1ce05aecf6
Aleks <aleks.koloch@gmail.com>
2024-01-31 22:35:44 +0100

    Add startQuest method to QuestSystem

--- src/systems/QuestSystem.cpp
+++ src/systems/QuestSystem.cpp
@@ -1,1 +1,7 @@
 #include "QuestSystem.h"
+
+void QuestSystem::startQuest(Quest quest)
+{
+    NotifySystem::notify(NotifySystem::Type::BigInfo, "Quest started: " + quest.name);
+    quests.push_back(quest);
+}
--- src/systems/QuestSystem.h
+++ src/systems/QuestSystem.h
@@ -4,9 +4,12 @@
 
 #include "QuestCore/Quest.h"
 
+#include "../systems/NotifySystem.h"
+
 class QuestSystem
 {
 public:
+    void startQuest(Quest quest);
 
 private:
     std::vector<Quest> quests;
