46f0af59410b4288de0dbf3962d881237fe67716
Aleks <aleks.koloch@gmail.com>
2024-01-31 22:48:56 +0100

    Add addRandomQuest method to QuestSystem

--- src/systems/QuestSystem.cpp
+++ src/systems/QuestSystem.cpp
@@ -5,3 +5,42 @@ void QuestSystem::startQuest(Quest quest)
     NotifySystem::notify(NotifySystem::Type::BigInfo, "Quest started: " + quest.name);
     quests.push_back(quest);
 }
+
+void QuestSystem::addRandomQuest(entt::registry& registry)
+{
+    QuestBuilder builder;
+
+    int stagesCount = ProceduralGenerationSystem::GetRandomNumber(3, 7);
+    
+    auto stageIndices = std::views::iota(0, stagesCount);
+    std::ranges::for_each(stageIndices, [&builder, &registry](int i) {
+        if (ProceduralGenerationSystem::GetRandomNumber(0, 1) == 0)
+        {
+            auto reachCondition = std::make_shared<ReachPointOfInterestCondition>("randomQuestPointOfInterest" + std::to_string(i));
+            QuestStage stage = QuestStageBuilder()
+                .addDescription("Find the point of interest")
+                .addAction([i](entt::registry& reg) {
+                    PointSystem::addPointOfInterest(reg, sf::Vector2f(ProceduralGenerationSystem::GetRandomNumber(-5000, 5000), ProceduralGenerationSystem::GetRandomNumber(-5000, 5000)), "randomQuestPointOfInterest" + std::to_string(i));
+                })
+                .addCondition(reachCondition)
+                .build();
+            builder.addStage(stage);
+        }
+        else
+        {
+            unsigned int enemiesCount = ProceduralGenerationSystem::GetRandomNumber(1, 10);
+            auto killCondition = std::make_shared<KillEnemiesCondition>(enemiesCount);
+            QuestStage stage = QuestStageBuilder()
+                .addDescription("Kill " + std::to_string(enemiesCount) + " enemies")
+                .addAction([](entt::registry& reg) {
+                })
+                .addCondition(killCondition)
+                .build();
+            builder.addStage(stage);
+        }
+    });
+
+    builder.addName("Random quest");
+    Quest quest = builder.build();
+    this->startQuest(quest);
+}
--- src/systems/QuestSystem.h
+++ src/systems/QuestSystem.h
@@ -5,11 +5,13 @@
 #include "QuestCore/Quest.h"
 
 #include "../systems/NotifySystem.h"
+#include "../systems/ProceduralGenerationSystem.h"
 
 class QuestSystem
 {
 public:
     void startQuest(Quest quest);
+    void addRandomQuest(entt::registry& registry);
 
 private:
     std::vector<Quest> quests;
