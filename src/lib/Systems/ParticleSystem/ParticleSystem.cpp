d8cd0e7d96f6b2263da2bd0d0bdc455321d4397d
Aleks <aleks.koloch@gmail.com>
2024-02-13 00:28:32 +0100

    Add ParticleSystem constructor

--- src/lib/Systems/ParticleSystem/ParticleSystem.cpp
+++ src/lib/Systems/ParticleSystem/ParticleSystem.cpp
@@ -1,2 +1,7 @@
 #include "pch.h"
 #include "ParticleSystem.h"
+
+ParticleSystem::ParticleSystem(entt::registry &registry) : registry(registry)
+{
+
+}
--- src/lib/Systems/ParticleSystem/ParticleSystem.h
+++ src/lib/Systems/ParticleSystem/ParticleSystem.h
@@ -1,6 +1,12 @@
 #pragma once
 
+#include <entt/entt.hpp>
+
 class ParticleSystem
 {
+public:
+    ParticleSystem(entt::registry& registry);
 
+private:
+    entt::registry& registry;
 };
