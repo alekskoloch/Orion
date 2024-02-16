975b485e4143844c5e43a0c5b46672c27d4c1e89
Aleks <aleks.koloch@gmail.com>
2024-02-16 21:27:13 +0100

    Add createFlameParticle to ParticleFactory

--- src/lib/Entities/ParticleFactory.cpp
+++ src/lib/Entities/ParticleFactory.cpp
@@ -1,2 +1,16 @@
 #include "pch.h"
+#include "ParticleFactory.h"
+
 #include "ParticleBuilder.h"
+
+Particle ParticleFactory::createFlameParticle(const sf::Vector2f& position, const sf::Vector2f& entityVelocity)
+{
+    return ParticleBuilder()
+        .addLifetime(0.25f)
+        .addRadius(3.f)
+        .addStartColor(sf::Color(255, 255, 0))
+        .addEndColor(sf::Color(255, 0, 0))
+        .addPosition(position)
+        .addVelocity(entityVelocity * 0.8f)
+        .build();
+}
--- src/lib/Entities/ParticleFactory.h
+++ src/lib/Entities/ParticleFactory.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include "particle.h"
+
 class ParticleFactory
 {
-
+public:
+    Particle createFlameParticle(const sf::Vector2f& position, const sf::Vector2f& entityVelocity);
 };
