cb2c99deb3c7e91964a5dfb9ce88f62716401cc4
Aleks <aleks.koloch@gmail.com>
2024-02-27 23:24:13 +0100

    Update GroupFormation with isFormationFull function

--- src/lib/Components/enemyGroup.h
+++ src/lib/Components/enemyGroup.h
@@ -6,6 +6,15 @@ struct GroupFormation
 {
     std::vector<std::vector<int>> formation;
     std::vector<std::vector<bool>> seats;
+
+    bool isFormationFull() const
+    {
+        for (auto& row : seats)
+            for (auto seat : row)
+                if (!seat)
+                    return false;
+        return true;
+    }
 };
 
 struct EnemyGroup
