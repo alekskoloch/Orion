be498b8acb6fe020e2d502495825720d7625f98b
Aleks <aleks.koloch@gmail.com>
2024-02-27 23:26:52 +0100

    Update GropuFormation with getCurrentPositionPriority function

--- src/lib/Components/enemyGroup.h
+++ src/lib/Components/enemyGroup.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <entt/entt.hpp>
 #include <vector>
+#include <algorithm>
 
 struct GroupFormation
 {
@@ -14,6 +16,32 @@ struct GroupFormation
                 if (!seat)
                     return false;
         return true;
+    }
+
+    int getCurrentPositionPriority()
+    {
+        if (isFormationFull())
+            return -1;
+
+        int maxPriority = 0;
+        for (const auto& row : formation) {
+            auto rowMax = *std::max_element(row.begin(), row.end());
+            if (rowMax > maxPriority) {
+                maxPriority = rowMax;
+            }
+        }
+
+        for (int priority = 1; priority <= maxPriority; ++priority) {
+            for (size_t i = 0; i < formation.size(); ++i) {
+                for (size_t j = 0; j < formation[i].size(); ++j) {
+                    if (formation[i][j] == priority && !seats[i][j]) {
+                        return priority;
+                    }
+                }
+            }
+        }
+
+        return -1; 
     }
 };
 
