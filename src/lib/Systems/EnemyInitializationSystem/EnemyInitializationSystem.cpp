522082d7135ef83dd81b1aca39b1e827ba159f7c
Aleks <aleks.koloch@gmail.com>
2024-06-17 12:49:30 +0200

    Fix spawnEnemy function

--- src/lib/Systems/EnemyInitializationSystem/EnemyInitializationSystem.cpp
+++ src/lib/Systems/EnemyInitializationSystem/EnemyInitializationSystem.cpp
@@ -47,43 +47,53 @@ entt::entity EnemyInitializationSystem::spawnEnemy(entt::registry& registry, sf:
     std::ifstream configFile(CONFIG_PATH + std::string("enemyConfig.json"));
     if (!configFile.is_open())
         throw std::runtime_error("Could not open config file");
-    else
+
+    nlohmann::json configJson;
+    configFile >> configJson;
+
+    if (!configJson.contains("enemies") || !configJson["enemies"].is_array())
+        throw std::runtime_error("Could not find enemies array in config file");
+
+    bool enemyFound = false;
+
+    for (const auto& enemyJson : configJson["enemies"])
     {
-        nlohmann::json configJson;
-        configFile >> configJson;
-
-        if (!configJson.contains("enemies") || !configJson["enemies"].is_array())
-            throw std::runtime_error("Could not find enemies array in config file");
-
-        for (const auto& enemyJson : configJson["enemies"])
+        if (enemyJson["name"] == enemyType)
         {
-            if (enemyJson["name"] == enemyType)
+            EnemyBuilder enemyBuilder(registry);
+
+            enemyBuilder.createEnemy(enemyJson["name"])
+                        .addEntityState(enemyJson["attackRange"], enemyJson["idleRange"])
+                        .addPosition(position)
+                        .addSpeed(enemyJson["speed"])
+                        .addHealth(enemyJson["health"])
+                        .addExperience(enemyJson["experience"])
+                        .addDrop(100, 69)
+                        .setWeapon(enemyJson["weapon"])
+                        .setWaypointMovement();
+
+            auto newEnemy = enemyBuilder.build();
+
+            if (!modificators.empty())
             {
-                EnemyBuilder enemyBuilder(registry);
-
-                enemyBuilder.createEnemy(enemyJson["name"])
-                            .addEntityState(enemyJson["attackRange"], enemyJson["idleRange"])
-                            .addPosition(position)
-                            .addSpeed(enemyJson["speed"])
-                            .addHealth(enemyJson["health"])
-                            .addExperience(enemyJson["experience"])
-                            .addDrop(100, 69)
-                            .setWeapon(enemyJson["weapon"])
-                            .setWaypointMovement();
-
-                auto newEnemy = enemyBuilder.build();
-
-                if (modificators.size() > 0)
+                auto& enemyModificator = registry.emplace<EnemyModificator>(newEnemy);
+                for (auto& modificator : modificators)
                 {
-                    auto enemyModificator = registry.emplace<EnemyModificator>(newEnemy);
-                    for (auto modificator : modificators)
-                    {
-                        enemyModificator.modificators.push_back(modificator);
-                    }
+                    enemyModificator.modificators.push_back(modificator);
                 }
             }
+
+            enemyFound = true;
+            return newEnemy;
         }
     }
+
+    if (!enemyFound)
+    {
+        throw std::runtime_error("Could not find enemy with name: " + enemyType);
+    }
+
+    return entt::entity{};
 }
 
 void EnemyInitializationSystem::drawEnemyModifiers(entt::registry& registry, entt::entity enemy)
