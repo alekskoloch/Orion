68f76c6d7060a1921d6d5db6b6634e5f5d5f4d7e
Aleks Koloch <aleks.koloch@gmail.com>
2026-01-22 22:47:33 +0100

    COMEBACK - style game class

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,59 +1,66 @@
-#include "pch.h"
 #include "Game.h"
+#include "pch.h"
 
+#include "ConfigManager.hpp"
 #include "SceneManager.h"
 #include "TextureManager.h"
-#include "ConfigManager.hpp"
 
-#include "PlayerInitializationSystem.h"
-#include "EnemyInitializationSystem.h"
-
-#include "nlohmann/json.hpp"
-
-Game::Game() : window(sf::VideoMode(ConfigManager::getInstance().getScreenWidth(), ConfigManager::getInstance().getScreenHeight()), "Orion", ConfigManager::getInstance().getWindowStyle()), systemManager(this->window, this->registry, this->event), guiManager(this->window, this->registry, this->event, this->systemManager.getQuests())    
+Game::Game()
+    : window( sf::VideoMode( ConfigManager::getInstance().getScreenWidth(),
+                             ConfigManager::getInstance().getScreenHeight() ),
+              "Orion", ConfigManager::getInstance().getWindowStyle() ),
+      systemManager( this->window, this->registry, this->event ),
+      guiManager( this->window, this->registry, this->event, this->systemManager.getQuests() )
 {
-    this->window.setFramerateLimit(ConfigManager::getInstance().getFrameRateLimit());
+    this->window.setFramerateLimit( ConfigManager::getInstance().getFrameRateLimit() );
 
     this->loadCursor();
 
-    SceneManager::getInstance().setCurrentScene(Scene::MainMenu);
+    SceneManager::getInstance().setCurrentScene( Scene::MainMenu );
 }
 
 void Game::loadCursor()
 {
-    sf::Vector2u hotspot;
+    sf::Vector2u hotSpot;
 
-    if (ConfigManager::getInstance().getScale() <= 0.25f)
+    constexpr auto cursorMinThreshold = 0.25F;
+    constexpr auto cursorMidThreshold = 0.5F;
+
+    constexpr auto cursorSmallSize = 6;
+    constexpr auto cursorMediumSize = 12;
+    constexpr auto cursorLargeSize = 25;
+
+    if ( ConfigManager::getInstance().getScale() <= cursorMinThreshold )
     {
-        this->cursorTexture = TextureManager::getInstance().getTexture("CursorSmall");
-        hotspot = {6, 6};
+        this->cursorTexture = TextureManager::getInstance().getTexture( "CursorSmall" );
+        hotSpot = { cursorSmallSize, cursorSmallSize };
     }
-    else if (ConfigManager::getInstance().getScale() <= 0.5f)
+    else if ( ConfigManager::getInstance().getScale() <= cursorMidThreshold )
     {
-        this->cursorTexture = TextureManager::getInstance().getTexture("CursorMedium");
-        hotspot = {12, 12};
+        this->cursorTexture = TextureManager::getInstance().getTexture( "CursorMedium" );
+        hotSpot = { cursorMediumSize, cursorMediumSize };
     }
     else
     {
-        this->cursorTexture = TextureManager::getInstance().getTexture("CursorLarge");
-        hotspot = {25, 25};
+        this->cursorTexture = TextureManager::getInstance().getTexture( "CursorLarge" );
+        hotSpot = { cursorLargeSize, cursorLargeSize };
     }
 
-
-    if (!this->cursor.loadFromPixels(this->cursorTexture.copyToImage().getPixelsPtr(), this->cursorTexture.getSize(), hotspot))
+    if ( !this->cursor.loadFromPixels( this->cursorTexture.copyToImage().getPixelsPtr(),
+                                       this->cursorTexture.getSize(), hotSpot ) )
     {
-        std::cerr << "Failed to load cursor texture" << std::endl;
+        std::cerr << "Failed to load cursor texture" << '\n';
     }
 
-    this->window.setMouseCursor(this->cursor);
+    this->window.setMouseCursor( this->cursor );
 }
 
 void Game::run()
 {
-    while (window.isOpen())
+    while ( window.isOpen() )
     {
         this->processEvents();
-        this->update(this->clock.restart());
+        this->update( this->clock.restart() );
         this->render();
     }
 }
@@ -62,40 +69,46 @@ void Game::processEvents()
 {
     this->event = sf::Event();
 
-    while (window.pollEvent(event))
+    while ( window.pollEvent( event ) )
     {
-        if (event.type == sf::Event::Closed)
+        if ( event.type == sf::Event::Closed )
+        {
             window.close();
+        }
 
-        if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Tab && !this->guiManager.pause())
+        if ( event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Tab &&
+             !this->guiManager.pause() )
         {
             this->systemManager.enableSlowMotion();
-            this->guiManager.toggleQuickMenu(true);
+            this->guiManager.toggleQuickMenu( true );
         }
-        if (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::Tab)
+        if ( event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::Tab )
         {
             this->systemManager.disableSlowMotion();
-            this->guiManager.toggleQuickMenu(false);
+            this->guiManager.toggleQuickMenu( false );
         }
 
-        //when ` key is pressed, toggle debug mode
-        if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Tilde)
+        if ( event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Tilde )
         {
             this->systemManager.debugMode = !this->systemManager.debugMode;
         }
     }
 
     this->guiManager.processInput();
-    
-    if (!this->guiManager.pause())
+
+    if ( !this->guiManager.pause() )
+    {
         this->systemManager.executeEventSystems();
+    }
 }
 
-void Game::update(sf::Time deltaTime)
+void Game::update( sf::Time deltaTime )
 {
-    this->guiManager.update(deltaTime);
-    if (!this->guiManager.pause())
-        this->systemManager.executeUpdateSystems(deltaTime);
+    this->guiManager.update( deltaTime );
+    if ( !this->guiManager.pause() )
+    {
+        this->systemManager.executeUpdateSystems( deltaTime );
+    }
 }
 
 void Game::render()
--- src/Game.h
+++ src/Game.h
@@ -5,24 +5,25 @@
 
 class Game
 {
+public:
+    Game();
+    void run();
+
 private:
     sf::RenderWindow window;
     entt::registry registry;
     sf::Clock clock;
-    sf::Event event;
+    sf::Event event{};
 
     SystemManager systemManager;
     GUIManager guiManager;
 
-    //TODO: this is only temporary cursor, create proper cursor class
+    // TODO: this is only temporary cursor, create proper cursor class
     void loadCursor();
     sf::Texture cursorTexture;
     sf::Cursor cursor;
 
     void processEvents();
-    void update(sf::Time deltaTime);
+    void update( sf::Time deltaTime );
     void render();
-public:
-    Game();
-    void run();
 };
