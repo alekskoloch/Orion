77873f470bd35e6d446308565dbb05e02d080ca2
Aleks Koloch <aleks.koloch@gmail.com>
2026-02-08 19:28:20 +0100

    InitScript - add VSCode extension step for clangd

--- initWindows.ps1
+++ initWindows.ps1
@@ -1,3 +1,6 @@
+# =========================================== VSCode step
+
+
 $VsCodeCommand = Get-Command "code" -ErrorAction SilentlyContinue
 
 if ($VSCodeCommand) {
@@ -12,5 +15,40 @@ if ($VSCodeCommand) {
 		Write-Host "Installation finished!" -ForegroundColor Green
 	} else {
 		Write-Host "Installation skipped by user." -ForegroundColor Gray
+	}
+}
+
+# =========================================== VSCode Extensions step
+
+$CodePath = "code"
+
+if (-not (Get-Command "code" -ErrorAction SilentlyContinue)) {
+    if (Test-Path "$env:LOCALAPPDATA\Programs\Microsoft VS Code\bin\code.cmd") {
+        $CodePath = "$env:LOCALAPPDATA\Programs\Microsoft VS Code\bin\code.cmd"
+    } elseif (Test-Path "$env:ProgramFiles\Microsoft VS Code\bin\code.cmd") {
+        $CodePath = "$env:ProgramFiles\Microsoft VS Code\bin\code.cmd"
+    } else {
+        Write-Host "Error: Could not find VS Code executable path." -ForegroundColor Red
+        exit
+    }
+}
+
+Write-Host "Checking VSCode extensions..." -ForegroundColor Cyan
+
+$InstalledExtensions = & $CodePath --list-extensions
+$ExtensionId = "llvm-vs-code-extensions.vscode-clangd"
+
+if ($InstalledExtensions -like "*$ExtensionId*") {
+	Write-Host "Extension '$ExtensionId' is already installed." -ForegroundColor Green
+} else {
+	Write-Host "Extension '$ExtensionId' is missing." -ForegroundColor Yellow
+	$UserInput = Read-Host "Do you want install '$ExtensionId'? (Y/N)"
+
+	if ($UserInput -eq 'Y') {
+		Write-Host "Installing extension '$ExtensionId'..." -ForegroundColor Cyan
+		& $CodePath --install-extension $ExtensionId --force
+		Write-Host "Extension installation finished!" -ForegroundColor Green
+	} else {
+		Write-Host "Extension installation skipped by user." -ForegroundColor Gray
 	}
 }
