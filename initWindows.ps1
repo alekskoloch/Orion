724ac13683390c5a03234fb69b5e33f6af27d17d
Aleks Koloch <aleks.koloch@gmail.com>
2026-02-08 19:05:21 +0100

    InitScript - add VSCode step

--- initWindows.ps1
+++ initWindows.ps1
@@ -3,5 +3,14 @@ $VsCodeCommand = Get-Command "code" -ErrorAction SilentlyContinue
 if ($VSCodeCommand) {
 	Write-Host "VS Code is currently installed." -ForegroundColor Green
 } else {
-	Write-Host "VS Code is NOT installed." -ForegroundColor Red
+	Write-Host "VS Code is missing." -ForegroundColor Yellow
+	$UserInput = Read-Host "Do you want install VS Code? (Y/N)"
+
+	if ($UserInput -eq 'Y') {
+		Write-Host "Starting VSCode installation process..." -ForegroundColor Cyan
+		winget install --id Microsoft.VisualStudioCode -e --silent --source winget --accept-package-agreements --accept-source-agreements
+		Write-Host "Installation finished!" -ForegroundColor Green
+	} else {
+		Write-Host "Installation skipped by user." -ForegroundColor Gray
+	}
 }
